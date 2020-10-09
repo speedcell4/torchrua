c36693ac3ff75558a3b2d62aa6f18c39237e2469
speedcell4 <speedcell4@gmail.com>
2020-10-10 01:37:00 +0900

    Feat: Init project

---
+++ README.md
@@ -0,0 +1,9 @@
+# TorchRua
+
+![Unit Tests](https://github.com/speedcell4/TorchRua/workflows/Unit%20Tests/badge.svg)
+
+*Rua* derives from the Szechwanese character <ruby>*挼*<rt>ruá</rt></ruby> which means "pack, rumple, screw up, manipulate". TorchRua provides tons of easy-to-use functions to help you rua variable-length Tensors with `PackedSequence`!
+
+## requirements
+
+- Python3.7 or higher
---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+name = 'torchrua'
+
+setup(
+    name=name,
+    version='0.1.0',
+    packages=[package for package in find_packages() if package.startswith(name)],
+    url='http://github.com/speedcell4/torchrua',
+    license='MIT',
+    author='speedcell4',
+    author_email='speedcell4@gmail.com',
+    description='Manipulate your variable-length Tensors',
+    python_requires='>=3.7',
+    install_requires=[
+        'numpy',
+    ],
+    extras_require={
+        'dev': [
+            'pytest',
+            'hypothesis',
+        ],
+        'benchmark': [
+            'aku',
+        ]
+    }
+)
