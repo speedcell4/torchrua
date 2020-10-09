846e353b97045d60085f0883b181e568a0a328ed
speedcell4 <speedcell4@gmail.com>
2020-10-10 03:35:46 +0900

    Feat: Add stack_packed_sequences

--- torchrua/mutating.py
+++ torchrua/mutating.py
@@ -37,7 +37,46 @@ def cat_packed_batch_sizes(pack: PackedSequence, num_packs: int) -> Tuple[Tensor
         unsorted_indices = torch.cat([
             pack.unsorted_indices * num_packs + index
             for index in range(num_packs)
-        ])
+        ], dim=0)
+    else:
+        unsorted_indices = None
+
+    return pack.batch_sizes * num_packs, sorted_indices, unsorted_indices
+
+
+def stack_packed_sequences(packs: List[PackedSequence]) -> PackedSequence:
+    data = stack_packed_data(packs=packs)
+    batch_sizes, sorted_indices, unsorted_indices = stack_packed_batch_sizes(pack=packs[0], num_packs=len(packs))
+
+    return PackedSequence(
+        data=data, batch_sizes=batch_sizes,
+        sorted_indices=sorted_indices,
+        unsorted_indices=unsorted_indices,
+    )
+
+
+def stack_packed_data(packs: List[PackedSequence]) -> Tensor:
+    data = torch.stack([pack.data for pack in packs], dim=1)
+    return rearrange(data, 'p n ... -> (p n) ...')
+
+
+@torch.no_grad()
+def stack_packed_batch_sizes(pack: PackedSequence, num_packs: int) -> Tuple[Tensor, Optional[Tensor], Optional[Tensor]]:
+    num_batches = pack.batch_sizes[0].item()
+
+    if pack.sorted_indices is not None:
+        sorted_indices = torch.stack([
+            pack.sorted_indices * num_packs + index
+            for index in range(num_packs)
+        ], dim=1).view(-1)
+    else:
+        sorted_indices = None
+
+    if pack.unsorted_indices is not None:
+        unsorted_indices = torch.cat([
+            pack.unsorted_indices + num_batches * index
+            for index in range(num_packs)
+        ], dim=0)
     else:
         unsorted_indices = None
 
