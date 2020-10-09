0a2e2da81066adfe5efb840fdba7f83073f5b89f
speedcell4 <speedcell4@gmail.com>
2020-10-10 06:22:41 +0900

    Feat: Add head_indices and select_head

--- torchrua/indexing.py
+++ torchrua/indexing.py
@@ -1,13 +1,18 @@
+import torch
 from torch import Tensor
-from torch.nn.utils.rnn import PackedSequence
+from torch.nn.utils.rnn import PackedSequence, pack_sequence
+from torch.nn.utils.rnn import pad_packed_sequence
 
 
-def head_indices(pack: PackedSequence) -> Tensor:
-    raise NotImplementedError
+@torch.no_grad()
+def head_indices(pack: PackedSequence, unsort: bool = True) -> Tensor:
+    if unsort and pack.unsorted_indices is not None:
+        return pack.unsorted_indices
+    return torch.arange(0, pack.batch_sizes[0].item(), dtype=torch.long, device=pack.data.device)
 
 
-def select_head(pack: PackedSequence) -> Tensor:
-    raise NotImplementedError
+def select_head(pack: PackedSequence, unsort: bool = True) -> Tensor:
+    return pack.data[head_indices(pack=pack, unsort=unsort)]
 
 
 def last_indices(pack: PackedSequence) -> Tensor:
@@ -40,3 +45,17 @@ def reverse_indices(pack: PackedSequence) -> Tensor:
 
 def flip_packed_sequence(pack: PackedSequence) -> PackedSequence:
     raise NotImplementedError
+
+
+if __name__ == '__main__':
+    x = pack_sequence([
+        torch.arange(5),
+        torch.arange(2) + 5,
+        torch.arange(3) + 5 + 2,
+    ], enforce_sorted=False)
+    data, _ = pad_packed_sequence(x, batch_first=True)
+    print(data)
+    print(head_indices(x, unsort=True))
+    print(select_head(x, unsort=True))
+    print(head_indices(x, unsort=False))
+    print(select_head(x, unsort=False))
