89d10112d34ef727773780446701d439fa4f11f1
speedcell4 <speedcell4@gmail.com>
2020-10-11 03:56:36 +0900

    Feat: Add lengths_to_batch_sizes

--- torchrua/padding.py
+++ torchrua/padding.py
@@ -1,8 +1,9 @@
-from typing import Union
+from typing import Union, Tuple
 
 import torch
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
+from torch.nn.utils.rnn import invert_permutation
 from torch.nn.utils.rnn import pad_packed_sequence
 
 
@@ -52,7 +53,11 @@ def pack_to_lengths(pack: PackedSequence, unsort: bool = True, *,
 def lengths_to_mask(lengths: Tensor, filling_mask: bool = True, *,
                     batch_first: bool = False,
                     dtype: torch.dtype = torch.bool, device: torch.device = None) -> Tensor:
-    indices = torch.arange(lengths.max().item(), dtype=lengths.dtype, device=lengths.device)
+    max_sent_length = lengths.max().item()
+    if device is None:
+        device = lengths.device
+
+    indices = torch.arange(max_sent_length, dtype=lengths.dtype, device=lengths.device)
 
     if filling_mask:
         op = torch.lt
@@ -65,3 +70,19 @@ def lengths_to_mask(lengths: Tensor, filling_mask: bool = True, *,
         mask = op(indices[:, None], lengths[None, :])
 
     return mask.to(dtype=dtype, device=device)
+
+
+@torch.no_grad()
+def lengths_to_batch_sizes(lengths: Tensor, *, device: torch.device = None) -> Tuple[Tensor, Tensor, Tensor]:
+    max_sent_length = lengths.max().item()
+    if device is None:
+        device = lengths.device
+
+    mask = torch.ones((max_sent_length,), dtype=torch.long, device=device)
+    mask = mask[:, None].expand((-1, max_sent_length)).tril(0)
+
+    batch_sizes = mask[lengths - 1].sum(dim=0)
+    sorted_indices = lengths.argsort(dim=0, descending=True)
+    unsorted_indices = invert_permutation(sorted_indices)
+
+    return batch_sizes, sorted_indices, unsorted_indices
