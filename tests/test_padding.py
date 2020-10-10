38ca4dea05e3e9daaa0f1c5017ab088fe383e069
speedcell4 <speedcell4@gmail.com>
2020-10-11 04:03:10 +0900

    Test: Add unit test for lengths_to_batch_sizes

--- tests/test_padding.py
+++ tests/test_padding.py
@@ -3,16 +3,16 @@ from hypothesis import given, strategies as st
 from torch.nn.utils.rnn import pack_sequence
 
 from tests.strategies import list_of_sentences
-from torchrua.padding import pack_to_lengths, pack_to_mask, lengths_to_mask
+from torchrua.padding import pack_to_lengths, pack_to_mask, lengths_to_mask, lengths_to_batch_sizes
 
 
 @given(
     sentences_and_lengths=list_of_sentences(return_lengths=True),
 )
 def test_pack_to_lengths(sentences_and_lengths):
-    sentences, x = sentences_and_lengths
+    sentences, lengths = sentences_and_lengths
 
-    x = torch.tensor(x, dtype=torch.long, device=sentences[0].device)
+    x = torch.tensor(lengths, dtype=torch.long, device=sentences[0].device)
     y = pack_to_lengths(
         pack=pack_sequence(sentences, enforce_sorted=False),
         unsort=True, dtype=torch.long,
@@ -22,13 +22,12 @@ def test_pack_to_lengths(sentences_and_lengths):
 
 
 @given(
-    sentences_and_lengths=list_of_sentences(),
+    sentences=list_of_sentences(),
     unsort=st.booleans(),
     batch_first=st.booleans(),
 
 )
-def test_lengths_to_mask(sentences_and_lengths, unsort, batch_first):
-    sentences = sentences_and_lengths
+def test_lengths_to_mask(sentences, unsort, batch_first):
     pack = pack_sequence(sentences, enforce_sorted=False)
 
     x = pack_to_mask(pack, unsort=unsort, batch_first=batch_first, dtype=torch.bool)
@@ -37,3 +36,16 @@ def test_lengths_to_mask(sentences_and_lengths, unsort, batch_first):
     y = lengths_to_mask(lengths, filling_mask=True, batch_first=batch_first, dtype=torch.bool)
 
     assert torch.equal(x, y), f'{x.contiguous().view(-1)} != {y.contiguous().view(-1)}'
+
+
+@given(
+    sentences=list_of_sentences(),
+)
+def test_lengths_to_batch_sizes(sentences):
+    lengths = torch.tensor([s.size(0) for s in sentences], dtype=torch.long, device=sentences[0].device)
+    batch_sizes, sorted_indices, unsorted_indices = lengths_to_batch_sizes(lengths=lengths, device=sentences[0].device)
+    y = pack_sequence(sentences, enforce_sorted=False)
+
+    assert torch.equal(batch_sizes, y.batch_sizes)
+    assert torch.equal(sorted_indices, y.sorted_indices)
+    assert torch.equal(unsorted_indices, y.unsorted_indices)
