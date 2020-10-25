f91c823b56f32a2cfabd78939e85e3654ecf7d4d
speedcell4 <speedcell4@gmail.com>
2020-10-25 13:13:26 +0900

    Benchmark: Add benchmark for pack_padded_sequence

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -1,11 +1,13 @@
 from aku import App
 
 from benchmark.cat_packed_sequence import cat_packed_sequence_fn
+from benchmark.pack_padded_sequence import pack_padded_sequence_fn
 from benchmark.reverse_packed_sequence import reverse_pack_fn
 
 app = App()
 
 app.register(reverse_pack_fn)
 app.register(cat_packed_sequence_fn)
+app.register(pack_padded_sequence_fn)
 
 app.run()
