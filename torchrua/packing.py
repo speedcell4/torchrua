178f0dc0abb1c945865731356e0064de4d30e108
speedcell4 <speedcell4@gmail.com>
2020-10-29 16:45:16 +0900

    Feat: Add pad_packed_sequence

--- torchrua/packing.py
+++ torchrua/packing.py
@@ -1,10 +1,12 @@
+from typing import Union, Tuple
+
 import torch
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
 from torch.nn.utils.rnn import invert_permutation
 
 from torchrua import batch_indices, token_indices
-from torchrua.utils import lengths_to_batch_sizes
+from torchrua.utils import lengths_to_batch_sizes, packed_sequence_to_lengths, fetch_batch_size, fetch_total_length
 
 
 def pack_padded_sequence(input: Tensor, lengths: Tensor,
@@ -37,3 +39,29 @@ def pack_padded_sequence(input: Tensor, lengths: Tensor,
         sorted_indices=sorted_indices,
         unsorted_indices=unsorted_indices,
     )
+
+
+def pad_packed_sequence(pack: PackedSequence, batch_first: bool = False,
+                        padding_value: Union[int, float, bool] = 0.0,
+                        total_length: int = None) -> Tuple[Tensor, Tensor]:
+    batch_size = fetch_batch_size(pack)
+    total_length = fetch_total_length(pack, total_length=total_length)
+
+    lengths = packed_sequence_to_lengths(pack=pack, unsort=True, dtype=torch.long, device=torch.device('cpu'))
+    batch_ptr = batch_indices(pack, device=pack.data.device)
+    token_ptr = token_indices(pack, device=pack.data.device)
+
+    if batch_first:
+        data = torch.full(
+            (batch_size, total_length), fill_value=padding_value,
+            device=pack.data.device, requires_grad=False,
+        )
+        data[batch_ptr, token_ptr] = pack.data
+    else:
+        data = torch.full(
+            (total_length, batch_size), fill_value=padding_value,
+            device=pack.data.device, requires_grad=False,
+        )
+        data[token_ptr, batch_ptr] = pack.data
+
+    return data, lengths
