d64511b19f9fe1ef2fd1204ab9659d380c087dc3
speedcell4 <speedcell4@gmail.com>
2020-10-29 23:59:54 +0900

    Chore: Update setup.py description

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ name = 'torchrua'
 
 setup(
     name=name,
-    version='0.1.0',
+    version='0.1.1',
     packages=[package for package in find_packages() if package.startswith(name)],
     url='http://github.com/speedcell4/torchrua',
     license='MIT',
     author='speedcell4',
     author_email='speedcell4@gmail.com',
-    description='Manipulate your variable-length Tensors',
+    description='Rua variable-length Tensors with PackedSequence!',
     python_requires='>=3.7',
     install_requires=[
         'numpy',
