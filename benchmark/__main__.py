9b4db143e0808e705ec8d9a424eaef91d95d1645
speedcell4 <speedcell4@gmail.com>
2021-04-06 23:52:51 +0900

    Refactor: Use new aku

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -1,4 +1,4 @@
-from aku import App
+from aku import Aku
 
 from benchmark.cat_packed_sequence import cat_pack
 from benchmark.pack_padded_sequence import pack_padded
@@ -6,12 +6,12 @@ from benchmark.pad_packed_sequence import pad_packed
 from benchmark.reverse_packed_sequence import reverse_pack
 from benchmark.roll_packed_sequence import roll_pack
 
-app = App()
+app = Aku()
 
-app.register(reverse_pack)
-app.register(roll_pack)
-app.register(cat_pack)
-app.register(pack_padded)
-app.register(pad_packed)
+app.option(reverse_pack)
+app.option(roll_pack)
+app.option(cat_pack)
+app.option(pack_padded)
+app.option(pad_packed)
 
 app.run()
