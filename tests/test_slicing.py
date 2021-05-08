143b559910fbed3a889e0cbdf4c8d20ca8033327
speedcell4 <speedcell4@gmail.com>
2021-05-08 14:25:26 +0900

    Refactor: Unify chunk_packed_sequence

--- tests/test_slicing.py
+++ tests/test_slicing.py
@@ -1,38 +1,25 @@
-from hypothesis import given
+from hypothesis import given, strategies as st
 from torch.nn.utils.rnn import pack_sequence, pad_packed_sequence
 
 from tests.strategies import list_of_homo_lists_of_sentences
 from tests.utils import assert_equal
+from torchrua import chunk_packed_sequence
 from torchrua import stack_packed_sequences
-from torchrua import uncat_packed_sequence, unstack_packed_sequence
 
 
 @given(
-    lists_of_sentences=list_of_homo_lists_of_sentences()
+    lists_of_sentences=list_of_homo_lists_of_sentences(),
+    dim=st.sampled_from([0, 1]),
 )
-def test_uncat_packed_sequence(lists_of_sentences):
+def test_chunk_packed_sequence(lists_of_sentences, dim):
     xs = [
         pack_sequence(sentences, enforce_sorted=False)
         for sentences in lists_of_sentences
     ]
-    ys = uncat_packed_sequence(pack=stack_packed_sequences(packs=xs, dim=1), num_packs=len(xs))
-
-    for x, y in zip(xs, ys):
-        x, _ = pad_packed_sequence(x, batch_first=True)
-        y, _ = pad_packed_sequence(y, batch_first=True)
-
-        assert_equal(x, y)
-
-
-@given(
-    lists_of_sentences=list_of_homo_lists_of_sentences()
-)
-def test_unstack_packed_sequence(lists_of_sentences):
-    xs = [
-        pack_sequence(sentences, enforce_sorted=False)
-        for sentences in lists_of_sentences
-    ]
-    ys = unstack_packed_sequence(pack=stack_packed_sequences(packs=xs, dim=0), num_packs=len(xs))
+    ys = chunk_packed_sequence(
+        pack=stack_packed_sequences(packs=xs, dim=dim),
+        chunks=len(xs), dim=dim,
+    )
 
     for x, y in zip(xs, ys):
         x, _ = pad_packed_sequence(x, batch_first=True)
--- torchrua/slicing.py
+++ torchrua/slicing.py
@@ -6,73 +6,58 @@ from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
 
 __all__ = [
-    'uncat_packed_sequence', 'uncat_packed_data', 'uncat_packed_batch_sizes',
-    'unstack_packed_sequence', 'unstack_packed_data', 'unstack_packed_batch_sizes',
+    'chunk_packed_sequence', 'chunk_packed_data', 'chunk_batch_sizes_dim0', 'chunk_batch_sizes_dim1',
 ]
 
 
-def uncat_packed_sequence(pack: PackedSequence, num_packs: int) -> List[PackedSequence]:
-    batch_sizes, sorted_indices, unsorted_indices = uncat_packed_batch_sizes(pack=pack, num_packs=num_packs)
+def chunk_packed_sequence(pack: PackedSequence, chunks: int, dim: int) -> List[PackedSequence]:
+    batch_sizes, sorted_indices, unsorted_indices = [
+        chunk_batch_sizes_dim0,
+        chunk_batch_sizes_dim1,
+    ][dim](pack=pack, chunks=chunks)
+
     return [
         PackedSequence(
-            data=data, batch_sizes=batch_sizes,
+            data=data,
+            batch_sizes=batch_sizes,
             sorted_indices=sorted_indices,
             unsorted_indices=unsorted_indices,
         )
-        for data in uncat_packed_data(pack=pack, num_packs=num_packs)
+        for data in chunk_packed_data(pack=pack, chunks=chunks)
     ]
 
 
-def uncat_packed_data(pack: PackedSequence, num_packs: int) -> List[Tensor]:
-    data = rearrange(pack.data, '(p n) ... -> p n ...', n=num_packs)
-    return [data[:, index] for index in range(num_packs)]
+def chunk_packed_data(pack: PackedSequence, chunks: int) -> List[Tensor]:
+    data = rearrange(pack.data, '(p n) ... -> p n ...', n=chunks)
+    return [data[:, index] for index in range(chunks)]
 
 
 @torch.no_grad()
-def uncat_packed_batch_sizes(pack: PackedSequence, num_packs: int) -> \
-        Tuple[Tensor, Optional[Tensor], Optional[Tensor]]:
+def chunk_batch_sizes_dim0(pack: PackedSequence, chunks: int) -> Tuple[Tensor, Optional[Tensor], Optional[Tensor]]:
     if pack.sorted_indices is not None:
-        sorted_indices = pack.sorted_indices[::num_packs]
-    else:
-        sorted_indices = None
-
-    num_batches = pack.batch_sizes[0].item() // num_packs
-    if pack.unsorted_indices is not None:
-        unsorted_indices = pack.unsorted_indices[:num_batches] // num_packs
-    else:
-        unsorted_indices = None
-
-    return pack.batch_sizes // num_packs, sorted_indices, unsorted_indices
-
-
-def unstack_packed_sequence(pack: PackedSequence, num_packs: int) -> List[PackedSequence]:
-    batch_sizes, sorted_indices, unsorted_indices = unstack_packed_batch_sizes(pack=pack, num_packs=num_packs)
-    return [
-        PackedSequence(
-            data=data, batch_sizes=batch_sizes,
-            sorted_indices=sorted_indices,
-            unsorted_indices=unsorted_indices,
-        )
-        for data in unstack_packed_data(pack=pack, num_packs=num_packs)
-    ]
-
-
-def unstack_packed_data(pack: PackedSequence, num_packs: int) -> List[Tensor]:
-    data = rearrange(pack.data, '(p n) ... -> p n ...', n=num_packs)
-    return [data[:, index] for index in range(num_packs)]
-
-
-@torch.no_grad()
-def unstack_packed_batch_sizes(pack: PackedSequence, num_packs: int) -> \
-        Tuple[Tensor, Optional[Tensor], Optional[Tensor]]:
-    if pack.sorted_indices is not None:
-        sorted_indices = pack.sorted_indices[::num_packs]
+        sorted_indices = pack.sorted_indices[::chunks]
     else:
         sorted_indices = None
 
     if pack.unsorted_indices is not None:
-        unsorted_indices = pack.unsorted_indices[::num_packs] // num_packs
+        unsorted_indices = pack.unsorted_indices[::chunks] // chunks
     else:
         unsorted_indices = None
 
-    return pack.batch_sizes // num_packs, sorted_indices, unsorted_indices
+    return pack.batch_sizes // chunks, sorted_indices, unsorted_indices
+
+
+@torch.no_grad()
+def chunk_batch_sizes_dim1(pack: PackedSequence, chunks: int) -> Tuple[Tensor, Optional[Tensor], Optional[Tensor]]:
+    if pack.sorted_indices is not None:
+        sorted_indices = pack.sorted_indices[::chunks]
+    else:
+        sorted_indices = None
+
+    num_batches = pack.batch_sizes[0].item() // chunks
+    if pack.unsorted_indices is not None:
+        unsorted_indices = pack.unsorted_indices[:num_batches] // chunks
+    else:
+        unsorted_indices = None
+
+    return pack.batch_sizes // chunks, sorted_indices, unsorted_indices
