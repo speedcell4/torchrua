f4d385d1ff43f89b455124838915230d41b64f78
speedcell4 <speedcell4@gmail.com>
2021-05-08 14:30:55 +0900

    Refactor: Update unit tests

--- tests/test_joining.py
+++ tests/test_joining.py
@@ -10,26 +10,7 @@ from torchrua import stack_packed_sequences
 @given(
     lists_of_sentences=list_of_homo_lists_of_sentences()
 )
-def test_cat_packed_sequences(lists_of_sentences):
-    xs = [
-        pack_sequence(sentences, enforce_sorted=False)
-        for sentences in lists_of_sentences
-    ]
-    x = stack_packed_sequences(sequences=xs, dim=1)
-    x, _ = pad_packed_sequence(x, batch_first=True)
-
-    y = pack_sequence([
-        s for sentences in lists_of_sentences for s in sentences
-    ], enforce_sorted=False)
-    y, _ = pad_packed_sequence(y, batch_first=True)
-
-    assert_equal(x, y)
-
-
-@given(
-    lists_of_sentences=list_of_homo_lists_of_sentences()
-)
-def test_stack_packed_sequences(lists_of_sentences):
+def test_stack_packed_sequences_dim0(lists_of_sentences):
     xs = [
         pack_sequence(sentences, enforce_sorted=False)
         for sentences in lists_of_sentences
@@ -39,6 +20,25 @@ def test_stack_packed_sequences(lists_of_sentences):
 
     y = pack_sequence([
         s for sentences in list(zip(*lists_of_sentences)) for s in sentences
+    ], enforce_sorted=False)
+    y, _ = pad_packed_sequence(y, batch_first=True)
+
+    assert_equal(x, y)
+
+
+@given(
+    lists_of_sentences=list_of_homo_lists_of_sentences()
+)
+def test_stack_packed_sequences_dim1(lists_of_sentences):
+    xs = [
+        pack_sequence(sentences, enforce_sorted=False)
+        for sentences in lists_of_sentences
+    ]
+    x = stack_packed_sequences(sequences=xs, dim=1)
+    x, _ = pad_packed_sequence(x, batch_first=True)
+
+    y = pack_sequence([
+        s for sentences in lists_of_sentences for s in sentences
     ], enforce_sorted=False)
     y, _ = pad_packed_sequence(y, batch_first=True)
 
