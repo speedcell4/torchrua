5b935bb04c1867e5c27fe60aa0ab53b5abe9538c
speedcell4 <speedcell4@gmail.com>
2021-06-07 19:55:21 +0900

    Feat: Add cat_padded_sequence

--- torchrua/catting.py
+++ torchrua/catting.py
@@ -4,7 +4,7 @@ import torch
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence, invert_permutation
 
-from torchrua.indexing import batch_sizes_to_ptr
+from torchrua.indexing import batch_sizes_to_ptr, lengths_to_ptr
 from torchrua.utils import accumulate_lengths, accumulate_batch_sizes
 
 __all__ = [
@@ -14,35 +14,56 @@ __all__ = [
 ]
 
 
-def cat_sequence(sequences: List[Tensor]) -> Tuple[Tensor, Tensor]:
-    data, lengths = zip(*[
-        (sequence, sequence.size()[0])
-        for sequence in sequences
-    ])
+def cat_sequence(sequences: List[Tensor], device: torch.device = None) -> Tuple[Tensor, Tensor]:
+    if device is None:
+        device = sequences[0].device
 
-    data = torch.cat(data, dim=0)
-    lengths = torch.tensor(lengths, dtype=torch.long, device=data.device)
+    data = torch.cat(sequences, dim=0)
+    lengths = torch.tensor([
+        sequence.size()[0] for sequence in sequences
+    ], dtype=torch.long, device=device)
     return data, lengths
 
 
-def cat_packed_sequence(sequence: PackedSequence) -> Tuple[Tensor, Tensor]:
-    device = sequence[0].data.device
+def cat_packed_sequence(sequence: PackedSequence, device: torch.device = None) -> Tuple[Tensor, Tensor]:
+    if device is None:
+        device = sequence[0].data.device
 
-    batch_ptr, token_ptr, lengths = batch_sizes_to_ptr(
+    batch_ptr, token_ptr, unsorted_lengths = batch_sizes_to_ptr(
         batch_sizes=sequence.batch_sizes,
         sorted_indices=None,
         unsorted_indices=sequence.unsorted_indices,
         total_length=None, device=device,
     )
     acc_batch_sizes = accumulate_batch_sizes(sequence.batch_sizes, device=device)
-    acc_lengths = accumulate_lengths(lengths)
+    acc_lengths = accumulate_lengths(unsorted_lengths)
 
     cat_index = acc_lengths[sequence.unsorted_indices[batch_ptr]] + token_ptr
     pack_index = acc_batch_sizes[token_ptr] + batch_ptr
     index = invert_permutation(cat_index)[pack_index]
 
-    return sequence.data[index], lengths
+    return sequence.data[index], unsorted_lengths
 
 
-def cat_padded_sequence(sequence: Tensor, lengths: Tensor, batch_first: bool) -> Tuple[Tensor, Tensor]:
-    raise NotImplementedError
+def cat_padded_sequence(sequence: Tensor, lengths: Tensor, batch_first: bool,
+                        device: torch.device = None) -> Tuple[Tensor, Tensor]:
+    if device is None:
+        device = sequence[0].device
+
+    cuda_unsorted_lengths = lengths.to(device=device)
+    batch_ptr, token_ptr, batch_sizes = lengths_to_ptr(
+        lengths=cuda_unsorted_lengths,
+        sorted_indices=None,
+        device=device,
+    )
+    acc_lengths = accumulate_lengths(cuda_unsorted_lengths)
+    index = invert_permutation(acc_lengths[batch_ptr] + token_ptr)
+    batch_ptr = batch_ptr[index]
+    token_ptr = token_ptr[index]
+
+    if batch_first:
+        data = sequence[batch_ptr, token_ptr]
+    else:
+        data = sequence[token_ptr, batch_ptr]
+
+    return data, cuda_unsorted_lengths
