6f8d9d733d3e1bd9be0e407d9e12fd572c5a4701
speedcell4 <speedcell4@gmail.com>
2021-06-17 23:23:54 +0900

    Refactor: Rewrite catting.py

--- torchrua/catting.py
+++ torchrua/catting.py
@@ -1,11 +1,11 @@
-from typing import List, Tuple
+from typing import List, Tuple, Optional
 
 import torch
 from torch import Tensor
-from torch.nn.utils.rnn import PackedSequence, invert_permutation
+from torch.nn.utils.rnn import PackedSequence
 
-from torchrua.indexing import batch_sizes_to_ptr, lengths_to_ptr
-from torchrua.utils import accumulate_lengths, accumulate_batch_sizes
+from torchrua.indexing import lengths_to_ptr, batch_sizes_to_ptr
+from torchrua.utils import accumulate_batch_sizes
 
 __all__ = [
     'cat_sequence',
@@ -14,58 +14,43 @@ __all__ = [
 ]
 
 
-def cat_sequence(sequences: List[Tensor], device: torch.device = None) -> Tuple[Tensor, Tensor]:
+def cat_sequence(sequences: List[Tensor], device: Optional[torch.device] = None) -> Tuple[Tensor, Tensor]:
     if device is None:
         device = sequences[0].device
 
-    data = torch.cat(sequences, dim=0)
-    lengths = torch.tensor([
-        sequence.size()[0] for sequence in sequences
-    ], dtype=torch.long, device=device)
+    data = torch.cat(sequences, dim=0).to(device=device)
+    lengths = torch.tensor([s.size()[0] for s in sequences], dtype=torch.long, device=device)
     return data, lengths
 
 
-def cat_packed_sequence(sequence: PackedSequence, device: torch.device = None) -> Tuple[Tensor, Tensor]:
+def cat_packed_sequence(pack: PackedSequence, device: Optional[torch.device] = None) -> Tuple[Tensor, Tensor]:
     with torch.no_grad():
         if device is None:
-            device = sequence[0].data.device
+            device = pack.data.device
 
-        batch_ptr, token_ptr, unsorted_lengths = batch_sizes_to_ptr(
-            batch_sizes=sequence.batch_sizes,
-            sorted_indices=None,
-            unsorted_indices=sequence.unsorted_indices,
-            total_length=None, device=device,
-        )
-        acc_batch_sizes = accumulate_batch_sizes(sequence.batch_sizes, device=device)
-        acc_lengths = accumulate_lengths(unsorted_lengths)
+        batch_sizes = pack.batch_sizes.to(device=device)
+        acc_batch_sizes = accumulate_batch_sizes(batch_sizes=batch_sizes)
+        token_ptr, batch_ptr, sorted_lengths = lengths_to_ptr(lengths=batch_sizes)
+        if pack.unsorted_indices is not None:
+            unsorted_lengths = sorted_lengths[pack.unsorted_indices]
 
-        cat_index = acc_lengths[sequence.unsorted_indices[batch_ptr]] + token_ptr
-        pack_index = acc_batch_sizes[token_ptr] + batch_ptr
-        index = invert_permutation(cat_index)[pack_index]
+        index = acc_batch_sizes[token_ptr] + batch_ptr
 
-    return sequence.data[index], unsorted_lengths
+    return pack.data[index], unsorted_lengths
 
 
-def cat_padded_sequence(sequence: Tensor, lengths: Tensor, batch_first: bool,
-                        device: torch.device = None) -> Tuple[Tensor, Tensor]:
+def cat_padded_sequence(tensor: Tensor, lengths: Tensor, batch_first: bool,
+                        device: Optional[torch.device] = None) -> Tuple[Tensor, Tensor]:
     with torch.no_grad():
         if device is None:
-            device = sequence[0].device
+            device = tensor[0].device
 
-        cuda_unsorted_lengths = lengths.to(device=device)
-        batch_ptr, token_ptr, batch_sizes = lengths_to_ptr(
-            lengths=cuda_unsorted_lengths,
-            sorted_indices=None,
-            device=device,
-        )
-        acc_lengths = accumulate_lengths(cuda_unsorted_lengths)
-        index = invert_permutation(acc_lengths[batch_ptr] + token_ptr)
-        batch_ptr = batch_ptr[index]
-        token_ptr = token_ptr[index]
+        lengths = lengths.to(device=device)
+        token_ptr, batch_ptr, _ = batch_sizes_to_ptr(lengths=lengths)
 
     if batch_first:
-        data = sequence[batch_ptr, token_ptr]
+        data = tensor[batch_ptr, token_ptr]
     else:
-        data = sequence[token_ptr, batch_ptr]
+        data = tensor[token_ptr, batch_ptr]
 
-    return data, cuda_unsorted_lengths
+    return data, lengths
