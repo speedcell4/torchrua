4351167973680d4afcbdb871ba539376fedde855
speedcell4 <speedcell4@gmail.com>
2021-06-19 00:39:12 +0900

    Test: Rewrite test_select_{head,last,init,tail}

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -1,100 +1,81 @@
 import torch
 from hypothesis import given, strategies as st
 from torch.nn.utils.rnn import pack_sequence
-from torch.nn.utils.rnn import pad_packed_sequence
 
-from tests.strategies import list_of_sentences
-from tests.utils import assert_equal
-from torchrua import select_head, select_last, select_init, select_tail, \
-    reverse_packed_sequence, roll_packed_sequence
+from tests.strategies import token_size_lists, embedding_dims, devices
+from tests.utils import assert_packed_close, assert_close
+from torchrua import select_head, select_last, select_init, select_tail
 
 
 @given(
-    sentences=list_of_sentences(),
+    data=st.data(),
+    token_sizes=token_size_lists(),
+    dim=embedding_dims(),
     unsort=st.booleans(),
+    device=devices(),
 )
-def test_select_head(sentences, unsort):
-    pack = pack_sequence(sentences, enforce_sorted=False)
-    x = select_head(pack=pack, unsort=unsort)
+def test_select_head(data, token_sizes, dim, unsort, device):
+    sequences = [torch.randn((token_size, dim), device=device) for token_size in token_sizes]
+    pack = pack_sequence(sequences, enforce_sorted=False)
 
-    y = torch.stack([s[0] for s in sentences], dim=0)
+    prd = select_head(pack=pack, unsort=unsort)
+
+    tgt = torch.stack([sequence[0] for sequence in sequences], dim=0)
     if not unsort:
-        y = y[pack.sorted_indices]
+        tgt = tgt[pack.sorted_indices]
 
-    assert_equal(x, y)
+    assert_close(prd, tgt)
 
 
 @given(
-    sentences=list_of_sentences(),
+    data=st.data(),
+    token_sizes=token_size_lists(),
+    dim=embedding_dims(),
     unsort=st.booleans(),
+    device=devices(),
 )
-def test_select_last(sentences, unsort):
-    pack = pack_sequence(sentences, enforce_sorted=False)
-    x = select_last(pack=pack, unsort=unsort)
+def test_select_last(data, token_sizes, dim, unsort, device):
+    sequences = [torch.randn((token_size, dim), device=device) for token_size in token_sizes]
+    pack = pack_sequence(sequences, enforce_sorted=False)
 
-    y = torch.stack([s[-1] for s in sentences], dim=0)
+    prd = select_last(pack=pack, unsort=unsort)
+
+    tgt = torch.stack([sequence[-1] for sequence in sequences], dim=0)
     if not unsort:
-        y = y[pack.sorted_indices]
+        tgt = tgt[pack.sorted_indices]
 
-    assert_equal(x, y)
-
-
-@given(
-    sentences=list_of_sentences(min_value=2),
-    drop_last_n=st.integers(1, max_value=11),
-)
-def test_select_init(sentences, drop_last_n):
-    pack = pack_sequence(sentences, enforce_sorted=False)
-    drop_last_n = min(drop_last_n, min(len(sent) for sent in sentences) - 1)
-    x = select_init(pack=pack, drop_last_n=drop_last_n)
-    x, _ = pad_packed_sequence(x, batch_first=True)
-
-    y = pack_sequence([s[:-drop_last_n] for s in sentences], enforce_sorted=False)
-    y, _ = pad_packed_sequence(y, batch_first=True)
-
-    assert_equal(x, y)
+    assert_close(prd, tgt)
 
 
 @given(
-    sentences=list_of_sentences(min_value=2),
-    drop_first_n=st.integers(1, max_value=11),
+    data=st.data(),
+    token_sizes=token_size_lists(),
+    dim=embedding_dims(),
+    device=devices(),
 )
-def test_select_tail(sentences, drop_first_n):
-    pack = pack_sequence(sentences, enforce_sorted=False)
-    drop_first_n = min(drop_first_n, min(len(sent) for sent in sentences) - 1)
-    x = select_tail(pack=pack, drop_first_n=drop_first_n)
-    x, _ = pad_packed_sequence(x, batch_first=True)
+def test_select_init(data, token_sizes, dim, device):
+    drop_last_n = data.draw(st.integers(min_value=1, max_value=min(token_sizes)))
+    sequences = [torch.randn((token_size + 1, dim), device=device) for token_size in token_sizes]
+    pack = pack_sequence(sequences, enforce_sorted=False)
 
-    y = pack_sequence([s[drop_first_n:] for s in sentences], enforce_sorted=False)
-    y, _ = pad_packed_sequence(y, batch_first=True)
+    prd = select_init(pack=pack, drop_last_n=drop_last_n)
+    tgt = pack_sequence([sequence[:-drop_last_n] for sequence in sequences], enforce_sorted=False)
 
-    assert_equal(x, y)
+    assert_packed_close(prd, tgt)
 
 
 @given(
-    sentences=list_of_sentences(),
+    data=st.data(),
+    token_sizes=token_size_lists(),
+    dim=embedding_dims(),
+    device=devices(),
 )
-def test_reverse_packed_sequence(sentences):
-    pack = pack_sequence(sentences, enforce_sorted=False)
-    x = reverse_packed_sequence(pack=pack)
-    x, _ = pad_packed_sequence(x, batch_first=True)
+def test_select_tail(data, token_sizes, dim, device):
+    drop_first_n = data.draw(st.integers(min_value=1, max_value=min(token_sizes)))
+    sequences = [torch.randn((token_size + 1, dim), device=device) for token_size in token_sizes]
+    pack = pack_sequence(sequences, enforce_sorted=False)
 
-    y = pack_sequence([s.flip(dims=[0]) for s in sentences], enforce_sorted=False)
-    y, _ = pad_packed_sequence(y, batch_first=True)
+    prd = select_tail(pack=pack, drop_first_n=drop_first_n)
+    tgt = pack_sequence([sequence[drop_first_n:] for sequence in sequences], enforce_sorted=False)
 
-    assert_equal(x, y)
-
-
-@given(
-    sentences=list_of_sentences(),
-    offset=st.integers(-5, +5),
-)
-def test_roll_packed_sequence(sentences, offset):
-    pack = pack_sequence(sentences, enforce_sorted=False)
-    x = roll_packed_sequence(pack=pack, offset=offset)
-    x, _ = pad_packed_sequence(x, batch_first=True)
-
-    y = pack_sequence([s.roll(offset, dims=[0]) for s in sentences], enforce_sorted=False)
-    y, _ = pad_packed_sequence(y, batch_first=True)
-
-    assert_equal(x, y)
+    assert_packed_close(prd, tgt)
