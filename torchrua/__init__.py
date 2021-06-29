2a9217daa23d6d4b671d8d9608e894475e29e5a5
speedcell4 <speedcell4@gmail.com>
2021-06-29 23:23:24 +0900

    Test: Add unit tests tree_reduction

--- torchrua/__init__.py
+++ torchrua/__init__.py
@@ -3,6 +3,7 @@ from torchrua.catting import *
 from torchrua.indexing import *
 from torchrua.joining import *
 from torchrua.packing import *
-from torchrua.reduction import *
+from torchrua.padding import *
 from torchrua.slicing import *
+from torchrua.tree_reduction import *
 from torchrua.utils import *
