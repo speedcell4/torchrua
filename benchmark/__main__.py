59c5525182bbe1f76cbb4a9b124e0a18001ed6ba
speedcell4 <speedcell4@gmail.com>
2021-06-30 00:00:07 +0900

    Benchmark: Add benchmarks for tree_reduction.py

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -2,7 +2,8 @@ from aku import Aku
 
 from benchmark.packing import pack_sequence, pack_padded_sequence, pack_catted_sequences
 from benchmark.padding import pad_sequence, pad_packed_sequence
-from benchmark.reduction import tree_reduce_packed_sequence
+from benchmark.tree_reduction import tree_reduce_packed_sequence, tree_reduce_padded_sequence, \
+    tree_reduce_catted_sequence
 
 app = Aku()
 
@@ -12,5 +13,7 @@ app.option(pad_sequence)
 app.option(pad_packed_sequence)
 app.option(pack_catted_sequences)
 app.option(tree_reduce_packed_sequence)
+app.option(tree_reduce_padded_sequence)
+app.option(tree_reduce_catted_sequence)
 
 app.run()
