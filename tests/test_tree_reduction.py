ecc3b70655d2acd66362c1f96a440d413333589a
speedcell4 <speedcell4@gmail.com>
2021-08-08 23:48:09 +0900

    Refactor: Update test_tree_reduction.py

--- tests/test_tree_reduction.py
+++ tests/test_tree_reduction.py
@@ -2,11 +2,11 @@ import torch
 from hypothesis import given, strategies as st
 
 from tests.strategies import token_size_lists, embedding_dims, devices
-from tests.utils import assert_close
+from tests.utils import assert_close, assert_grad_close
 from torchrua import pack_sequence, cat_sequence
 from torchrua.padding import pad_sequence
-from torchrua.tree_reduction import tree_reduce_packed_indices, tree_reduce_padded_indices
 from torchrua.tree_reduction import tree_reduce_catted_indices, tree_reduce_sequence
+from torchrua.tree_reduction import tree_reduce_packed_indices, tree_reduce_padded_indices
 
 
 @given(
@@ -16,19 +16,21 @@ from torchrua.tree_reduction import tree_reduce_catted_indices, tree_reduce_sequ
     device=devices(),
 )
 def test_tree_reduce_packed_sequence(data, token_sizes, dim, device):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    target = pad_sequence(sequences, device=device).sum(dim=0)
+    excepted = pad_sequence(inputs, device=device).sum(dim=0)
 
-    sequence = pack_sequence(sequences, device=device)
-    indices = tree_reduce_packed_indices(batch_sizes=sequence.batch_sizes)
-    prediction = tree_reduce_sequence(torch.add)(sequence.data, indices)
-    prediction = prediction[sequence.unsorted_indices]
+    packed_sequence = pack_sequence(inputs, device=device)
+    indices = tree_reduce_packed_indices(batch_sizes=packed_sequence.batch_sizes)
 
-    assert_close(prediction, target)
+    actual = tree_reduce_sequence(torch.add)(packed_sequence.data, indices)
+    actual = actual[packed_sequence.unsorted_indices]
+
+    assert_close(actual, excepted)
+    assert_grad_close(actual, excepted, inputs=inputs)
 
 
 @given(
@@ -39,19 +41,20 @@ def test_tree_reduce_packed_sequence(data, token_sizes, dim, device):
     device=devices(),
 )
 def test_tree_reduce_padded_sequence(data, token_sizes, dim, batch_first, device):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    target = pad_sequence(sequences, device=device).sum(dim=0)
+    excepted = pad_sequence(inputs, device=device).sum(dim=0)
 
-    sequence = pad_sequence(sequences, device=device, batch_first=batch_first)
+    padded_sequence = pad_sequence(inputs, device=device, batch_first=batch_first)
     token_sizes = torch.tensor(token_sizes, device=device)
     indices = tree_reduce_padded_indices(token_sizes=token_sizes, batch_first=batch_first)
-    prediction = tree_reduce_sequence(torch.add)(sequence.data, indices)
+    actual = tree_reduce_sequence(torch.add)(padded_sequence.data, indices)
 
-    assert_close(prediction, target)
+    assert_close(actual, excepted)
+    # assert_grad_close(actual, excepted, inputs=inputs) TODO
 
 
 @given(
@@ -61,15 +64,16 @@ def test_tree_reduce_padded_sequence(data, token_sizes, dim, batch_first, device
     device=devices(),
 )
 def test_tree_reduce_catted_sequence(data, token_sizes, dim, device):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    target = pad_sequence(sequences, device=device).sum(dim=0)
+    excepted = pad_sequence(inputs, device=device).sum(dim=0)
 
-    sequence, token_sizes = cat_sequence(sequences, device=device)
+    catted_sequence, token_sizes = cat_sequence(inputs, device=device)
     indices = tree_reduce_catted_indices(token_sizes=token_sizes)
-    prediction = tree_reduce_sequence(torch.add)(sequence.data, indices)
+    actual = tree_reduce_sequence(torch.add)(catted_sequence.data, indices)
 
-    assert_close(prediction, target)
+    assert_close(actual, excepted)
+    # assert_grad_close(actual, excepted, inputs=inputs) TODO
