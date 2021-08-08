242ce5df8237e96385e6473dfcffcc592e01b2b1
speedcell4 <speedcell4@gmail.com>
2021-08-08 23:32:25 +0900

    Refactor: Update test_cat_packed_sequence

--- tests/test_catting.py
+++ tests/test_catting.py
@@ -14,15 +14,15 @@ from torchrua import catting as rua
     device=devices(),
 )
 def test_cat_packed_sequence(data, token_sizes, dim, device):
-    sequences = [torch.randn((token_size, dim), device=device, requires_grad=True) for token_size in token_sizes]
-    packed_sequence = tgt.pack_sequence(sequences, enforce_sorted=False)
+    inputs = [torch.randn((token_size, dim), device=device, requires_grad=True) for token_size in token_sizes]
+    packed_sequence = tgt.pack_sequence(inputs, enforce_sorted=False)
 
-    data_actual, token_sizes_actual = rua.cat_sequence(sequences, device=device)
-    data_expected, token_sizes_expected = rua.cat_packed_sequence(packed_sequence, device=device)
+    actual_data, actual_token_sizes = rua.cat_sequence(inputs, device=device)
+    expected_data, expected_token_sizes = rua.cat_packed_sequence(packed_sequence, device=device)
 
-    assert_close(data_actual, data_expected)
-    assert_equal(token_sizes_actual, token_sizes_expected)
-    assert_grad_close(data_actual, data_expected, inputs=sequences)
+    assert_close(actual_data, expected_data)
+    assert_equal(actual_token_sizes, expected_token_sizes)
+    assert_grad_close(actual_data, expected_data, inputs=inputs)
 
 
 @given(
