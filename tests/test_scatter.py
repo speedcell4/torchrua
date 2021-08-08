0a16f1142a332c05d685348626c00be0c47c063a
speedcell4 <speedcell4@gmail.com>
2021-08-08 23:41:50 +0900

    Refactor: Update test_scatter.py

--- tests/test_scatter.py
+++ tests/test_scatter.py
@@ -19,14 +19,14 @@ def test_scatter_add(data, token_size, num, dim, device):
     if num > token_size:
         num, token_size = token_size, num
 
-    tensor = torch.randn((token_size, dim), device=device, requires_grad=True)
+    inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
     index = torch.randint(0, num, (token_size,), device=device)
 
-    prediction = rua_scatter.scatter_add(tensor=tensor, index=index)
-    target = torch_scatter.scatter_add(src=tensor, index=index, dim=0)
+    actual = rua_scatter.scatter_add(tensor=inputs, index=index)
+    excepted = torch_scatter.scatter_add(src=inputs, index=index, dim=0)
 
-    assert_close(actual=prediction, expected=target)
-    assert_grad_close(actual=prediction, expected=target, inputs=(tensor,))
+    assert_close(actual, excepted)
+    assert_grad_close(actual, excepted, inputs=inputs)
 
 
 @given(
@@ -40,14 +40,14 @@ def test_scatter_mean(data, token_size, num, dim, device):
     if num > token_size:
         num, token_size = token_size, num
 
-    tensor = torch.randn((token_size, dim), device=device, requires_grad=True)
+    inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
     index = torch.randint(0, num, (token_size,), device=device)
 
-    prediction = rua_scatter.scatter_mean(tensor=tensor, index=index)
-    target = torch_scatter.scatter_mean(src=tensor, index=index, dim=0)
+    actual = rua_scatter.scatter_mean(tensor=inputs, index=index)
+    excepted = torch_scatter.scatter_mean(src=inputs, index=index, dim=0)
 
-    assert_close(actual=prediction, expected=target)
-    assert_grad_close(actual=prediction, expected=target, inputs=(tensor,))
+    assert_close(actual=actual, expected=excepted)
+    assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
 
 
 @given(
@@ -61,14 +61,14 @@ def test_scatter_max(data, token_size, num, dim, device):
     if num > token_size:
         num, token_size = token_size, num
 
-    tensor = torch.randn((token_size, dim), device=device, requires_grad=True)
+    inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
     index = torch.randint(0, num, (token_size,), device=device)
 
-    prediction = rua_scatter.scatter_max(tensor=tensor, index=index)
-    target, _ = torch_scatter.scatter_max(src=tensor, index=index, dim=0)
+    actual = rua_scatter.scatter_max(tensor=inputs, index=index)
+    excepted, _ = torch_scatter.scatter_max(src=inputs, index=index, dim=0)
 
-    assert_close(actual=prediction, expected=target)
-    assert_grad_close(actual=prediction, expected=target, inputs=(tensor,))
+    assert_close(actual=actual, expected=excepted)
+    assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
 
 
 @given(
@@ -82,11 +82,11 @@ def test_scatter_min(data, token_size, num, dim, device):
     if num > token_size:
         num, token_size = token_size, num
 
-    tensor = torch.randn((token_size, dim), device=device, requires_grad=True)
+    inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
     index = torch.randint(0, num, (token_size,), device=device)
 
-    prediction = rua_scatter.scatter_min(tensor=tensor, index=index)
-    target, _ = torch_scatter.scatter_min(src=tensor, index=index, dim=0)
+    actual = rua_scatter.scatter_min(tensor=inputs, index=index)
+    excepted, _ = torch_scatter.scatter_min(src=inputs, index=index, dim=0)
 
-    assert_close(actual=prediction, expected=target)
-    assert_grad_close(actual=prediction, expected=target, inputs=(tensor,))
+    assert_close(actual=actual, expected=excepted)
+    assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
