193b1f638b327c2f202e801f538b05822eceb9f0
speedcell4 <speedcell4@gmail.com>
2021-08-08 20:03:09 +0900

    Benchmark: Add scatter_softmax

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -3,7 +3,7 @@ from aku import Aku
 from benchmark.packing import pack_sequence, pack_padded_sequence
 from benchmark.padding import pad_sequence, pad_packed_sequence
 from benchmark.reduction import reduce_catted_sequences
-from benchmark.scatter import scatter_add
+from benchmark.scatter import scatter_add, scatter_softmax
 from benchmark.tree_reduction import tree_reduce_catted_sequence
 from benchmark.tree_reduction import tree_reduce_packed_sequence, tree_reduce_padded_sequence
 
@@ -18,5 +18,6 @@ app.option(tree_reduce_packed_sequence)
 app.option(tree_reduce_padded_sequence)
 app.option(tree_reduce_catted_sequence)
 app.option(scatter_add)
+app.option(scatter_softmax)
 
 app.run()
--- benchmark/scatter.py
+++ benchmark/scatter.py
@@ -6,6 +6,7 @@ import torch_scatter
 from benchmark.generators import draw_devices, draw_embedding_dims, draw_token_sizes
 from benchmark.utils import timeit, TimerSuit
 from torchrua.scatter import scatter_add as rua_scatter_add
+from torchrua.scatter import scatter_softmax as rua_scatter_softmax
 
 
 @timeit
@@ -27,6 +28,39 @@ def scatter_add(device: Type[draw_devices],
 
     with timer.naive_forward:
         target = torch_scatter.scatter_add(src=tensor, index=index, dim=0)
+
+    with timer.rua_backward:
+        torch.autograd.grad(
+            prediction, tensor, torch.ones_like(prediction),
+            create_graph=False,
+        )
+
+    with timer.naive_backward:
+        torch.autograd.grad(
+            target, tensor, torch.ones_like(target),
+            create_graph=False,
+        )
+
+
+@timeit
+def scatter_softmax(device: Type[draw_devices],
+                    token_sizes: Type[draw_token_sizes],
+                    dim: Type[draw_embedding_dims], *,
+                    timer: TimerSuit):
+    device = device()
+    token_size, num = token_sizes(), token_sizes()
+    if num > token_size:
+        token_size, num = num, token_size
+    in_dim = dim()
+
+    tensor = torch.randn((token_size, in_dim), requires_grad=True, device=device)
+    index = torch.randint(0, num, (token_size,), device=device)
+
+    with timer.rua_forward:
+        prediction = rua_scatter_softmax(tensor=tensor, index=index)
+
+    with timer.naive_forward:
+        target = torch_scatter.scatter_softmax(src=tensor, index=index, dim=0)
 
     with timer.rua_backward:
         torch.autograd.grad(
