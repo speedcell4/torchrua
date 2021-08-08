38ce71a7acbe9cc752d0e3efd49a1794a3f99a0c
speedcell4 <speedcell4@gmail.com>
2021-08-09 00:58:51 +0900

    Test: Add unit test for scatter_mul

--- tests/test_scatter.py
+++ tests/test_scatter.py
@@ -22,10 +22,33 @@ try:
             num, token_size = token_size, num
 
         inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
-        index = torch.randint(0, num, (token_size,), device=device)
+        index1 = torch.randint(0, num, (token_size,), device=device)
+        index2 = index1[:, None].expand_as(inputs)
 
-        actual = rua_scatter.scatter_add(tensor=inputs, index=index)
-        excepted = torch_scatter.scatter_add(src=inputs, index=index, dim=0)
+        actual = rua_scatter.scatter_add(tensor=inputs, index=index1)
+        excepted = torch_scatter.scatter_add(src=inputs, index=index2, dim=0)
+
+        assert_close(actual, excepted)
+        assert_grad_close(actual, excepted, inputs=inputs)
+
+
+    @given(
+        data=st.data(),
+        token_size=token_sizes(),
+        num=token_sizes(),
+        dim=embedding_dims(),
+        device=devices(),
+    )
+    def test_scatter_mul(data, token_size, num, dim, device):
+        if num > token_size:
+            num, token_size = token_size, num
+
+        inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
+        index1 = torch.randint(0, num, (token_size,), device=device)
+        index2 = index1[:, None].expand_as(inputs)
+
+        actual = rua_scatter.scatter_mul(tensor=inputs, index=index1)
+        excepted = torch_scatter.scatter_mul(src=inputs, index=index2, dim=0)
 
         assert_close(actual, excepted)
         assert_grad_close(actual, excepted, inputs=inputs)
@@ -43,10 +66,11 @@ try:
             num, token_size = token_size, num
 
         inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
-        index = torch.randint(0, num, (token_size,), device=device)
+        index1 = torch.randint(0, num, (token_size,), device=device)
+        index2 = index1[:, None].expand_as(inputs)
 
-        actual = rua_scatter.scatter_mean(tensor=inputs, index=index)
-        excepted = torch_scatter.scatter_mean(src=inputs, index=index, dim=0)
+        actual = rua_scatter.scatter_mean(tensor=inputs, index=index1)
+        excepted = torch_scatter.scatter_mean(src=inputs, index=index2, dim=0)
 
         assert_close(actual=actual, expected=excepted)
         assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
@@ -64,10 +88,11 @@ try:
             num, token_size = token_size, num
 
         inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
-        index = torch.randint(0, num, (token_size,), device=device)
+        index1 = torch.randint(0, num, (token_size,), device=device)
+        index2 = index1[:, None].expand_as(inputs)
 
-        actual = rua_scatter.scatter_max(tensor=inputs, index=index)
-        excepted, _ = torch_scatter.scatter_max(src=inputs, index=index, dim=0)
+        actual = rua_scatter.scatter_max(tensor=inputs, index=index1)
+        excepted, _ = torch_scatter.scatter_max(src=inputs, index=index2, dim=0)
 
         assert_close(actual=actual, expected=excepted)
         assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
@@ -85,10 +110,11 @@ try:
             num, token_size = token_size, num
 
         inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
-        index = torch.randint(0, num, (token_size,), device=device)
+        index1 = torch.randint(0, num, (token_size,), device=device)
+        index2 = index1[:, None].expand_as(inputs)
 
-        actual = rua_scatter.scatter_min(tensor=inputs, index=index)
-        excepted, _ = torch_scatter.scatter_min(src=inputs, index=index, dim=0)
+        actual = rua_scatter.scatter_min(tensor=inputs, index=index1)
+        excepted, _ = torch_scatter.scatter_min(src=inputs, index=index2, dim=0)
 
         assert_close(actual=actual, expected=excepted)
         assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
@@ -106,10 +132,11 @@ try:
             num, token_size = token_size, num
 
         inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
-        index = torch.randint(0, num, (token_size,), device=device)
+        index1 = torch.randint(0, num, (token_size,), device=device)
+        index2 = index1[:, None].expand_as(inputs)
 
-        actual = rua_scatter.scatter_logsumexp(tensor=inputs, index=index)
-        excepted = torch_scatter.scatter_logsumexp(src=inputs, index=index, dim=0)
+        actual = rua_scatter.scatter_logsumexp(tensor=inputs, index=index1)
+        excepted = torch_scatter.scatter_logsumexp(src=inputs, index=index2, dim=0)
 
         mask = ~torch.isinf(excepted)
         actual = torch.masked_select(actual, mask=mask)
@@ -131,10 +158,11 @@ try:
             num, token_size = token_size, num
 
         inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
-        index = torch.randint(0, num, (token_size,), device=device)
+        index1 = torch.randint(0, num, (token_size,), device=device)
+        index2 = index1[:, None].expand_as(inputs)
 
-        actual = rua_scatter.scatter_softmax(tensor=inputs, index=index)
-        excepted = torch_scatter.scatter_softmax(src=inputs, index=index, dim=0)
+        actual = rua_scatter.scatter_softmax(tensor=inputs, index=index1)
+        excepted = torch_scatter.scatter_softmax(src=inputs, index=index2, dim=0)
 
         assert_close(actual=actual, expected=excepted)
         assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
@@ -152,10 +180,11 @@ try:
             num, token_size = token_size, num
 
         inputs = torch.randn((token_size, dim), device=device, requires_grad=True)
-        index = torch.randint(0, num, (token_size,), device=device)
+        index1 = torch.randint(0, num, (token_size,), device=device)
+        index2 = index1[:, None].expand_as(inputs)
 
-        actual = rua_scatter.scatter_log_softmax(tensor=inputs, index=index)
-        excepted = torch_scatter.scatter_log_softmax(src=inputs, index=index, dim=0)
+        actual = rua_scatter.scatter_log_softmax(tensor=inputs, index=index1)
+        excepted = torch_scatter.scatter_log_softmax(src=inputs, index=index2, dim=0)
 
         assert_close(actual=actual, expected=excepted)
         assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
