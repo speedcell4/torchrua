6380513093967f14076970a8aee2662110d4fc90
speedcell4 <speedcell4@gmail.com>
2021-08-08 17:38:15 +0900

    Feat: Add scatter_add, scatter_mean, and scatter_max

--- torchrua/scatter.py
+++ torchrua/scatter.py
@@ -2,32 +2,63 @@ from typing import Tuple
 
 import torch
 from torch import Tensor
-from torch.nn.utils.rnn import invert_permutation
 from torch.types import Device
+
+from torchrua import accumulate_sizes
 
 __all__ = [
     'scatter_index_to_ptr',
+    'scatter_add', 'scatter_mean', 'scatter_max', 'scatter_min',
 ]
 
 
 @torch.no_grad()
-def scatter_index_to_ptr(index: Tensor, keep_order: bool = True,
+def scatter_index_to_ptr(index: Tensor,
                          dtype: torch.dtype = torch.long,
-                         device: Device = None) -> Tuple[Tensor, Tensor, Tensor, Tensor, Tensor]:
+                         device: Device = None) -> Tuple[Tensor, Tensor]:
     if device is None:
         device = index.device
 
-    sorted_index, sorted_indices = torch.sort(index, dim=0, stable=True, descending=False)
-    unsorted_indices = invert_permutation(sorted_indices)
+    index = index.to(dtype=dtype, device=device)
+    _, sorted_indices = torch.sort(index, dim=0, stable=True, descending=False)
 
-    other_ptr = torch.arange(index.max().item() + 1, dtype=dtype, device=device)
-    tb_mask = other_ptr[:, None] == index[None, :]
-    acc_mask = tb_mask.to(dtype=dtype).cumsum(dim=1)
+    token_sizes = torch.zeros((index.max().item() + 1,), dtype=dtype, device=device)
+    token_sizes.scatter_add_(dim=0, index=index, src=torch.ones_like(index))
 
-    token_sizes = acc_mask[:, -1]
-    other_ptr = torch.masked_select(acc_mask, mask=tb_mask) - 1
+    return sorted_indices, accumulate_sizes(sizes=token_sizes)
 
-    if not keep_order:
-        return sorted_index, other_ptr, sorted_indices, unsorted_indices, token_sizes
-    else:
-        return index, other_ptr[unsorted_indices], sorted_indices, unsorted_indices, token_sizes
+
+def scatter_add(tensor: Tensor, index: Tensor) -> Tensor:
+    indices, offsets = scatter_index_to_ptr(index=index, device=tensor.device)
+    ret, _, _, _ = torch.embedding_bag(
+        weight=tensor.view((tensor.size()[0], -1)),
+        indices=indices, offsets=offsets, mode=0,
+    )
+    return ret.view((ret.size()[0], *tensor.size()[1:]))
+
+
+def scatter_mean(tensor: Tensor, index: Tensor) -> Tensor:
+    indices, offsets = scatter_index_to_ptr(index=index, device=tensor.device)
+    ret, _, _, _ = torch.embedding_bag(
+        weight=tensor.view((tensor.size()[0], -1)),
+        indices=indices, offsets=offsets, mode=1,
+    )
+    return ret.view((ret.size()[0], *tensor.size()[1:]))
+
+
+def scatter_max(tensor: Tensor, index: Tensor) -> Tensor:
+    indices, offsets = scatter_index_to_ptr(index=index, device=tensor.device)
+    ret, _, _, _ = torch.embedding_bag(
+        weight=tensor.view((tensor.size()[0], -1)),
+        indices=indices, offsets=offsets, mode=2,
+    )
+    return ret.view((ret.size()[0], *tensor.size()[1:]))
+
+
+def scatter_min(tensor: Tensor, index: Tensor) -> Tensor:
+    indices, offsets = scatter_index_to_ptr(index=index, device=tensor.device)
+    ret, _, _, _ = torch.embedding_bag(
+        weight=tensor.neg().view((tensor.size()[0], -1)),
+        indices=indices, offsets=offsets, mode=2,
+    )
+    return ret.neg().view((ret.size()[0], *tensor.size()[1:]))
