096aaf71dbca7cfdcc048c84f88cb23978f83edd
speedcell4 <speedcell4@gmail.com>
2021-08-08 23:33:47 +0900

    Refactor: Update test_cat_padded_sequence

--- tests/test_catting.py
+++ tests/test_catting.py
@@ -14,7 +14,10 @@ from torchrua import catting as rua
     device=devices(),
 )
 def test_cat_packed_sequence(data, token_sizes, dim, device):
-    inputs = [torch.randn((token_size, dim), device=device, requires_grad=True) for token_size in token_sizes]
+    inputs = [
+        torch.randn((token_size, dim), device=device, requires_grad=True)
+        for token_size in token_sizes
+    ]
     packed_sequence = tgt.pack_sequence(inputs, enforce_sorted=False)
 
     actual_data, actual_token_sizes = rua.cat_sequence(inputs, device=device)
@@ -33,14 +36,17 @@ def test_cat_packed_sequence(data, token_sizes, dim, device):
     device=devices(),
 )
 def test_cat_padded_sequence(data, token_sizes, dim, batch_first, device):
-    sequences = [torch.randn((token_size, dim), device=device, requires_grad=True) for token_size in token_sizes]
-    padded_sequence = tgt.pad_sequence(sequences, batch_first=batch_first)
+    inputs = [
+        torch.randn((token_size, dim), device=device, requires_grad=True)
+        for token_size in token_sizes
+    ]
+    padded_sequence = tgt.pad_sequence(inputs, batch_first=batch_first)
     token_sizes = torch.tensor(token_sizes, device=device)
 
-    data_actual, token_sizes_actual = rua.cat_sequence(sequences, device=device)
-    data_expected, token_sizes_expected = rua.cat_padded_sequence(
+    actual_data, actual_token_sizes = rua.cat_sequence(inputs, device=device)
+    expected_data, expected_token_sizes = rua.cat_padded_sequence(
         padded_sequence, token_sizes, batch_first=batch_first, device=device)
 
-    assert_close(data_actual, data_expected)
-    assert_equal(token_sizes_actual, token_sizes_expected)
-    assert_grad_close(data_actual, data_expected, inputs=sequences)
+    assert_close(actual_data, expected_data)
+    assert_equal(actual_token_sizes, expected_token_sizes)
+    assert_grad_close(actual_data, expected_data, inputs=inputs)
