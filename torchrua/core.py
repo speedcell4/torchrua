a260419e8df8ce56ce29e0f452392b99ec8611f3
speedcell4 <speedcell4@gmail.com>
2022-01-28 21:34:01 +0900

    Feat: Add transpose_sizes

--- torchrua/core.py
+++ torchrua/core.py
@@ -16,3 +16,13 @@ def sizes_to_ptr(sizes: Tensor, reverse: bool = False) -> Tuple[Tensor, Tensor]:
     else:
         minor_ptr = acc_sizes[major_ptr] - index - 1
     return major_ptr, minor_ptr
+
+
+@torch.no_grad()
+def transpose_sizes(sizes: Tensor) -> Tensor:
+    sorted_sizes, _ = torch.sort(sizes, descending=True)
+
+    rolled_sizes = torch.roll(sorted_sizes, -1, dims=0)
+    rolled_sizes[-1] = 0
+
+    return torch.repeat_interleave(sorted_sizes - rolled_sizes).add(1).flip(dims=[0])
