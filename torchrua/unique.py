7ed1647db67e95c498a083f7fcc6cd5e1ed50f9b
speedcell4 <speedcell4@gmail.com>
2022-02-14 23:36:09 +0900

    Feat: Add unique_packed_sequence

--- torchrua/unique.py
+++ torchrua/unique.py
@@ -2,9 +2,10 @@ from typing import Tuple
 
 import torch
 from torch import Tensor
+from torch.nn.utils.rnn import PackedSequence
 from torch.types import Device
 
-from torchrua import CattedSequence
+from torchrua import CattedSequence, batch_sizes_to_ptr
 
 
 def unique_catted_sequence(sequence: CattedSequence, device: Device = None) -> Tuple[Tensor, Tensor, Tensor]:
@@ -24,3 +25,23 @@ def unique_catted_sequence(sequence: CattedSequence, device: Device = None) -> T
     )
 
     return unique1[unique2 % n], inverse_ptr, counts
+
+
+def unique_packed_sequence(sequence: PackedSequence, device: Device = None) -> Tuple[Tensor, Tensor, Tensor]:
+    if device is None:
+        device = sequence.data.device
+
+    data = sequence.data.to(device=device)
+    batch_sizes = sequence.batch_sizes.to(device=device)
+
+    unique1, token_ptr, _ = torch.unique(data, sorted=True, return_inverse=True, return_counts=True)
+    _, batch_ptr, _ = batch_sizes_to_ptr(batch_sizes=batch_sizes)
+
+    n = unique1.size()[0]
+    unique2, inverse_ptr, counts = torch.unique(
+        n * batch_ptr + token_ptr,
+        sorted=True, return_inverse=True, return_counts=True,
+    )
+
+    return unique1[unique2 % n], inverse_ptr, counts
+
