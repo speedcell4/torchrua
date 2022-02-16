7dce48aabdce8ea6b479699c0401c7f97a736c24
speedcell4 <speedcell4@gmail.com>
2022-02-16 21:44:21 +0900

    Feat: Update reversed_catted_indices

--- torchrua/reverse.py
+++ torchrua/reverse.py
@@ -1,23 +1,31 @@
 import torch
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
+from torch.types import Device
 
-from torchrua import CattedSequence, accumulate_sizes, batch_sizes_to_ptr
+from torchrua.catting import CattedSequence
 from torchrua.core import sizes_to_ptr, transpose_sizes
+from torchrua.utils import accumulate_sizes
 
 
 @torch.no_grad()
-def reversed_catted_indices(sequence: CattedSequence) -> Tensor:
-    token_sizes = sequence.token_sizes.to(device=sequence.data.device)
-    acc_token_sizes = accumulate_sizes(sizes=token_sizes)
-    batch_ptr, token_ptr, _ = batch_sizes_to_ptr(batch_sizes=token_sizes)
+def reversed_catted_indices(token_sizes: Tensor, device: Device = None) -> Tensor:
+    if device is None:
+        device = token_sizes.device
 
-    token_ptr = token_sizes[batch_ptr] - token_ptr - 1
-    return token_ptr + acc_token_sizes[batch_ptr]
+    token_sizes = token_sizes.to(device=device)
+    acc_token_sizes = accumulate_sizes(sizes=token_sizes)
+
+    batch_ptr = torch.repeat_interleave(token_sizes)
+    token_ptr = torch.repeat_interleave(token_sizes.cumsum(dim=0) - 1, token_sizes)
+    token_ptr = token_ptr - torch.arange(token_ptr.size()[0], device=token_ptr.device)
+
+    return acc_token_sizes[batch_ptr] + token_ptr
 
 
 def reverse_catted_sequence(sequence: CattedSequence) -> CattedSequence:
-    indices = reversed_catted_indices(sequence)
+    indices = reversed_catted_indices(sequence.token_sizes, device=sequence.data.device)
+
     return CattedSequence(
         data=sequence.data[indices],
         token_sizes=sequence.token_sizes,
