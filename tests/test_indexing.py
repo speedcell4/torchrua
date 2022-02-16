63a2e78fed1f9bc47330c7ed50f25340a48804c6
speedcell4 <speedcell4@gmail.com>
2022-02-16 22:51:37 +0900

    Test: Update test_head_packed_sequence

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -1,6 +1,6 @@
 import torch
 from hypothesis import given, strategies as st
-from torch.nn.utils.rnn import pack_sequence
+from torchrua.packing import pack_sequence
 
 from tests.strategies import token_size_lists, embedding_dims, devices
 from tests.utils import assert_packed_sequence_close, assert_close, assert_grad_close
@@ -8,27 +8,26 @@ from torchrua.indexing import head_packed_sequence, last_packed_sequence, init_p
 
 
 @given(
-    data=st.data(),
     token_sizes=token_size_lists(),
     dim=embedding_dims(),
     unsort=st.booleans(),
     device=devices(),
 )
-def test_select_head(data, token_sizes, dim, unsort, device):
-    inputs = [
+def test_head_packed_sequence(token_sizes, dim, unsort, device):
+    sequences = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
-    packed_pack_sequence = pack_sequence(inputs, enforce_sorted=False)
+    packed_sequence = pack_sequence(sequences)
 
-    actual = head_packed_sequence(sequence=packed_pack_sequence, unsort=unsort)
+    actual = head_packed_sequence(sequence=packed_sequence, unsort=unsort)
     if not unsort:
-        actual = actual[packed_pack_sequence.unsorted_indices]
+        actual = actual[packed_sequence.unsorted_indices]
 
-    expected = torch.stack([sequence[0] for sequence in inputs], dim=0)
+    expected = torch.stack([sequence[0] for sequence in sequences], dim=0)
 
     assert_close(actual, expected)
-    assert_grad_close(actual, expected, inputs=inputs)
+    assert_grad_close(actual, expected, inputs=sequences)
 
 
 @given(
@@ -97,5 +96,3 @@ def test_select_tail(data, token_sizes, dim, device):
 
     assert_packed_sequence_close(actual, expected)
     assert_grad_close(actual.data, expected.data, inputs=inputs)
-
-
