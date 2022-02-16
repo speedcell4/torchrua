a97999c67ae9285ac992d6e580f1cfa2b76d6287
speedcell4 <speedcell4@gmail.com>
2022-02-16 22:47:56 +0900

    Refactor: Update init_catted_indices

--- torchrua/selecting/catting.py
+++ torchrua/selecting/catting.py
@@ -5,13 +5,13 @@ from torch import Tensor
 from torch.types import Device
 
 from torchrua.catting import CattedSequence
-from torchrua.core import batch_sizes_to_ptr
+from torchrua.core import batch_sizes_to_ptr, major_sizes_to_ptr
 from torchrua.utils import accumulate_sizes
 
 __all__ = [
     'head_catted_indices', 'head_catted_sequence',
     'last_catted_indices', 'last_catted_sequence',
-    'init_catted_mask', 'init_catted_sequence',
+    'init_catted_indices', 'init_catted_sequence',
     'tail_catted_mask', 'tail_catted_sequence',
 ]
 
@@ -45,14 +45,24 @@ def last_catted_sequence(sequence: CattedSequence) -> Tensor:
 
 
 @torch.no_grad()
-def init_catted_mask(sequence: CattedSequence, n: int = 1) -> Tuple[Tensor, Tensor]:
-    assert (sequence.token_sizes >= n).all().item()
+def init_catted_indices(token_sizes: Tensor, n: int = 1, device: Device = None) -> Tuple[Tensor, Tensor]:
+    if device is None:
+        device = token_sizes.device
 
-    token_sizes = sequence.token_sizes.to(device=sequence.data.device)
-    batch_ptr, token_ptr, _ = batch_sizes_to_ptr(batch_sizes=token_sizes)
+    token_sizes = token_sizes.to(device=device)
+    acc_token_sizes = accumulate_sizes(sizes=token_sizes)
+    token_ptr, batch_ptr = major_sizes_to_ptr(sizes=token_sizes - n)
 
-    token_sizes = token_sizes - n
-    return token_ptr < token_sizes[batch_ptr], token_sizes
+    return token_ptr + acc_token_sizes[batch_ptr]
+
+
+def init_catted_sequence(sequence: CattedSequence, n: int = 1) -> CattedSequence:
+    indices = init_catted_indices(token_sizes=sequence.token_sizes, n=n, device=sequence.data.device)
+
+    return CattedSequence(
+        data=sequence.data[indices],
+        token_sizes=sequence.token_sizes - n,
+    )
 
 
 @torch.no_grad()
@@ -63,11 +73,6 @@ def tail_catted_mask(sequence: CattedSequence, n: int = 1) -> Tuple[Tensor, Tens
     batch_ptr, token_ptr, _ = batch_sizes_to_ptr(batch_sizes=token_sizes)
 
     return token_ptr >= n, token_sizes - n
-
-
-def init_catted_sequence(sequence: CattedSequence, n: int = 1) -> CattedSequence:
-    indices, token_sizes = init_catted_mask(sequence=sequence, n=n)
-    return CattedSequence(data=sequence.data[indices], token_sizes=token_sizes)
 
 
 def tail_catted_sequence(sequence: CattedSequence, n: int = 1) -> CattedSequence:
