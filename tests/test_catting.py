fb5660d696733bc0eb03c60055b55f77aca41f3e
speedcell4 <speedcell4@gmail.com>
2022-02-24 14:50:37 +0900

    Refactor: Update test_catting.py

--- tests/test_catting.py
+++ tests/test_catting.py
@@ -1,9 +1,10 @@
 import torch
 from hypothesis import given, strategies as st
-from torch.nn.utils.rnn import pack_sequence, pad_sequence
+from torch.nn.utils.rnn import pack_sequence as torch_pack_sequence
+from torch.nn.utils.rnn import pad_sequence as torch_pad_sequence
 
 from tests.strategies import draw_token_sizes, draw_embedding_dim, draw_device
-from tests.utils import assert_close, assert_equal, assert_grad_close
+from tests.utils import assert_grad_close, assert_catted_sequence_close
 from torchrua.catting import cat_sequence, cat_padded_sequence, cat_packed_sequence
 
 
@@ -17,14 +18,13 @@ def test_cat_packed_sequence(token_sizes, dim, device):
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
-    packed_sequence = pack_sequence(inputs, enforce_sorted=False)
+    packed_sequence = torch_pack_sequence(inputs, enforce_sorted=False)
 
-    actual_data, actual_token_sizes = cat_sequence(inputs, device=device)
-    expected_data, expected_token_sizes = cat_packed_sequence(packed_sequence, device=device)
+    actual = cat_sequence(inputs, device=device)
+    expected = cat_packed_sequence(packed_sequence, device=device)
 
-    assert_close(actual_data, expected_data)
-    assert_equal(actual_token_sizes, expected_token_sizes)
-    assert_grad_close(actual_data, expected_data, inputs=inputs)
+    assert_catted_sequence_close(actual=actual, expected=expected)
+    assert_grad_close(actual=actual.data, expected=expected.data, inputs=inputs)
 
 
 @given(
@@ -38,13 +38,11 @@ def test_cat_padded_sequence(token_sizes, dim, batch_first, device):
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
-    padded_sequence = pad_sequence(inputs, batch_first=batch_first)
+    sequence = torch_pad_sequence(inputs, batch_first=batch_first)
     token_sizes = torch.tensor(token_sizes, device=device)
 
-    actual_data, actual_token_sizes = cat_sequence(inputs, device=device)
-    expected_data, expected_token_sizes = cat_padded_sequence(
-        padded_sequence, token_sizes, batch_first=batch_first, device=device)
+    actual = cat_sequence(inputs, device=device)
+    expected = cat_padded_sequence(sequence, token_sizes, batch_first=batch_first, device=device)
 
-    assert_close(actual_data, expected_data)
-    assert_equal(actual_token_sizes, expected_token_sizes)
-    assert_grad_close(actual_data, expected_data, inputs=inputs)
+    assert_catted_sequence_close(actual=actual, expected=expected)
+    assert_grad_close(actual=actual.data, expected=expected.data, inputs=inputs)
