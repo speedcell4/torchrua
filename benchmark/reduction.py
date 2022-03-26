5bfd57929b75467099d8742de69ba29bb26221bc
speedcell4 <speedcell4@gmail.com>
2022-03-27 00:16:25 +0900

    Benchmark: Add benchmarks for new reduce_*_sequence

--- benchmark/reduction.py
+++ benchmark/reduction.py
@@ -8,6 +8,7 @@ from torchrua import cat_sequence, reduce_catted_indices, pad_catted_sequence
 from torchrua import pack_sequence, reduce_packed_indices
 from torchrua import pad_sequence, reduce_padded_indices, pad_packed_sequence
 from torchrua import reduce_sequence
+from torchrua.reduction import reduce_packed_indices2, reduce_sequence2, reduce_padded_indices2, reduce_catted_indices2
 
 
 @timeit
@@ -19,30 +20,67 @@ def reduce_packed_sequence(token_sizes: Type[draw_token_size_lists],
     dim = dim()
     device = device()
 
-    inputs = pack_sequence([
+    sequence = data, batch_sizes, _, _ = pack_sequence([
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ], device=device)
 
     with timer.rua_compile:
-        indices = reduce_packed_indices(batch_sizes=inputs.batch_sizes)
+        indices = reduce_packed_indices(batch_sizes=batch_sizes)
 
     with timer.rua_forward:
-        actual = reduce_sequence(torch.add)(inputs.data, indices)
+        actual = reduce_sequence(torch.add)(data, indices)
 
     with timer.naive_forward:
-        excepted, _ = pad_packed_sequence(inputs, batch_first=False)
+        excepted, _ = pad_packed_sequence(sequence, batch_first=False)
         excepted = excepted.sum(dim=0)
 
     with timer.rua_backward:
         _ = torch.autograd.grad(
-            actual, inputs.data, torch.ones_like(actual),
+            actual, data, torch.ones_like(actual),
             create_graph=False,
         )
 
     with timer.naive_backward:
         _ = torch.autograd.grad(
-            excepted, inputs.data, torch.ones_like(excepted),
+            excepted, data, torch.ones_like(excepted),
+            create_graph=False,
+        )
+
+
+@timeit
+def reduce_packed_sequence2(token_sizes: Type[draw_token_size_lists],
+                            dim: Type[draw_embedding_dims],
+                            device: Type[draw_devices], *,
+                            timer: TimerSuit):
+    token_sizes = token_sizes()
+    dim = dim()
+    device = device()
+
+    sequence = data, batch_sizes, _, _ = pack_sequence([
+        torch.randn((token_size, dim), device=device, requires_grad=True)
+        for token_size in token_sizes
+    ], device=device)
+
+    with timer.rua_compile:
+        indices = reduce_packed_indices2(batch_sizes=batch_sizes)
+
+    with timer.rua_forward:
+        actual = reduce_sequence2(data, indices, op=torch.add)
+
+    with timer.naive_forward:
+        excepted, _ = pad_packed_sequence(sequence, batch_first=False)
+        excepted = excepted.sum(dim=0)
+
+    with timer.rua_backward:
+        _ = torch.autograd.grad(
+            actual, data, torch.ones_like(actual),
+            create_graph=False,
+        )
+
+    with timer.naive_backward:
+        _ = torch.autograd.grad(
+            excepted, data, torch.ones_like(excepted),
             create_graph=False,
         )
 
@@ -56,30 +94,65 @@ def reduce_padded_sequence(token_sizes: Type[draw_token_size_lists],
     dim = dim()
     device = device()
 
-    sequences = pad_sequence([
+    data, token_sizes = pad_sequence([
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ], device=device, batch_first=False)
-    token_sizes = torch.tensor(token_sizes, device=device)
 
     with timer.rua_compile:
         indices = reduce_padded_indices(token_sizes=token_sizes, batch_first=False)
 
     with timer.rua_forward:
-        prediction = reduce_sequence(torch.add)(sequences, indices)
+        prediction = reduce_sequence(torch.add)(data, indices)
 
     with timer.naive_forward:
-        target = sequences.sum(dim=0)
+        target = data.sum(dim=0)
 
     with timer.rua_backward:
         _ = torch.autograd.grad(
-            prediction, sequences, torch.ones_like(prediction),
+            prediction, data, torch.ones_like(prediction),
             create_graph=False,
         )
 
     with timer.naive_backward:
         _ = torch.autograd.grad(
-            target, sequences, torch.ones_like(target),
+            target, data, torch.ones_like(target),
+            create_graph=False,
+        )
+
+
+@timeit
+def reduce_padded_sequence2(token_sizes: Type[draw_token_size_lists],
+                            dim: Type[draw_embedding_dims],
+                            device: Type[draw_devices], *,
+                            timer: TimerSuit):
+    token_sizes = token_sizes()
+    dim = dim()
+    device = device()
+
+    data, token_sizes = pad_sequence([
+        torch.randn((token_size, dim), device=device, requires_grad=True)
+        for token_size in token_sizes
+    ], device=device, batch_first=False)
+
+    with timer.rua_compile:
+        indices = reduce_padded_indices2(token_sizes=token_sizes, batch_first=False)
+
+    with timer.rua_forward:
+        prediction = reduce_sequence2(data, indices, op=torch.add)
+
+    with timer.naive_forward:
+        target = data.sum(dim=0)
+
+    with timer.rua_backward:
+        _ = torch.autograd.grad(
+            prediction, data, torch.ones_like(prediction),
+            create_graph=False,
+        )
+
+    with timer.naive_backward:
+        _ = torch.autograd.grad(
+            target, data, torch.ones_like(target),
             create_graph=False,
         )
 
@@ -93,16 +166,16 @@ def reduce_catted_sequence(token_sizes: Type[draw_token_size_lists],
     dim = dim()
     device = device()
 
-    sequence = cat_sequence([
+    sequence = data, token_sizes = cat_sequence([
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ], device=device)
 
     with timer.rua_compile:
-        indices = reduce_catted_indices(token_sizes=sequence.token_sizes)
+        indices = reduce_catted_indices(token_sizes=token_sizes)
 
     with timer.rua_forward:
-        prediction = reduce_sequence(torch.add)(sequence, indices)
+        prediction = reduce_sequence(torch.add)(data, indices)
 
     with timer.naive_forward:
         target, _ = pad_catted_sequence(sequence, batch_first=False)
@@ -110,12 +183,49 @@ def reduce_catted_sequence(token_sizes: Type[draw_token_size_lists],
 
     with timer.rua_backward:
         _ = torch.autograd.grad(
-            prediction, sequence, torch.ones_like(prediction),
+            prediction, sequence.data, torch.ones_like(prediction),
             create_graph=False,
         )
 
     with timer.naive_backward:
         _ = torch.autograd.grad(
-            target, sequence, torch.ones_like(target),
+            target, sequence.data, torch.ones_like(target),
+            create_graph=False,
+        )
+
+
+@timeit
+def reduce_catted_sequence2(token_sizes: Type[draw_token_size_lists],
+                            dim: Type[draw_embedding_dims],
+                            device: Type[draw_devices], *,
+                            timer: TimerSuit):
+    token_sizes = token_sizes()
+    dim = dim()
+    device = device()
+
+    sequence = data, token_sizes = cat_sequence([
+        torch.randn((token_size, dim), device=device, requires_grad=True)
+        for token_size in token_sizes
+    ], device=device)
+
+    with timer.rua_compile:
+        indices = reduce_catted_indices2(token_sizes=token_sizes)
+
+    with timer.rua_forward:
+        prediction = reduce_sequence2(data, indices, op=torch.add)
+
+    with timer.naive_forward:
+        target, _ = pad_catted_sequence(sequence, batch_first=False)
+        target = target.sum(dim=0)
+
+    with timer.rua_backward:
+        _ = torch.autograd.grad(
+            prediction, data, torch.ones_like(prediction),
+            create_graph=False,
+        )
+
+    with timer.naive_backward:
+        _ = torch.autograd.grad(
+            target, data, torch.ones_like(target),
             create_graph=False,
         )
