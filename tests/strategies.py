7f6aa39e44af52128b07a3a9a065158150b19a61
speedcell4 <speedcell4@gmail.com>
2022-05-27 21:17:41 +0900

    Feat: Update sizes strategy

--- tests/strategies.py
+++ tests/strategies.py
@@ -2,17 +2,17 @@ import torch
 from hypothesis import strategies as st
 
 TINY_BATCH_SIZE = 5
-TINY_TOKEN_SIZE = 10
-TINY_EMBEDDING_DIM = 12
+TINY_TOKEN_SIZE = 11
+TINY_EMBEDDING_DIM = 13
 
 if torch.cuda.is_available():
-    BATCH_SIZE = 50
-    TOKEN_SIZE = 80
-    EMBEDDING_DIM = 100
+    BATCH_SIZE = 53
+    TOKEN_SIZE = 83
+    EMBEDDING_DIM = 107
 else:
-    BATCH_SIZE = 30
-    TOKEN_SIZE = 50
-    EMBEDDING_DIM = 60
+    BATCH_SIZE = 37
+    TOKEN_SIZE = 53
+    EMBEDDING_DIM = 61
 
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
@@ -30,4 +30,4 @@ def sizes(draw, *size: int, min_size: int = 1):
     if len(size) == 0:
         return n
     else:
-        return [draw(sizes(*size, min_size=min_size)) for _ in range(n)]
+        return draw(st.lists(sizes(*size), min_size=n, max_size=n))
