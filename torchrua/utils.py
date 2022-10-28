02af6614cbd3ecd2cd84ffcf5f7d7355fe81af67
speedcell4 <speedcell4@gmail.com>
2022-10-28 23:49:17 +0900

    Feat: Add sequence_ptr

--- torchrua/utils.py
+++ torchrua/utils.py
@@ -1,9 +1,16 @@
 from functools import singledispatch
 from typing import Tuple, Union
 
+from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
+from torch.types import Device
 
-from torchrua import CattedSequence, major_sizes_to_size
+from torchrua import CattedSequence, major_sizes_to_size, major_sizes_to_ptr
+
+__all__ = [
+    'sequence_shape', 'catted_sequence_shape', 'packed_sequence_shape',
+    'sequence_ptr', 'catted_sequence_ptr', 'packed_sequence_ptr',
+]
 
 
 @singledispatch
@@ -23,3 +30,26 @@ def packed_sequence_shape(sequence: PackedSequence) -> Tuple[int, int, int]:
     b, t = major_sizes_to_size(sizes=sequence.batch_sizes)
     n, *_ = sequence.data.size()
     return n, b, t
+
+
+@singledispatch
+def sequence_ptr(sequence: Union[CattedSequence, PackedSequence], device: Device = None) -> Tuple[Tensor, Tensor]:
+    raise TypeError(f'type {type(sequence)} is not supported')
+
+
+@sequence_ptr.register
+def catted_sequence_ptr(sequence: CattedSequence, device: Device = None) -> Tuple[Tensor, Tensor]:
+    if device is None:
+        device = sequence.data.device
+
+    token_ptr, batch_ptr = major_sizes_to_ptr(sizes=sequence.token_sizes.to(device=device))
+    return batch_ptr, token_ptr
+
+
+@sequence_ptr.register
+def packed_sequence_ptr(sequence: PackedSequence, device: Device = None) -> Tuple[Tensor, Tensor]:
+    if device is None:
+        device = sequence.data.device
+
+    batch_ptr, token_ptr = major_sizes_to_ptr(sizes=sequence.batch_sizes.to(device=device))
+    return batch_ptr, token_ptr
