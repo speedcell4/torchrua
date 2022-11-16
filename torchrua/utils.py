03086c23044d2486c79a57eb2b3b73ec697ffa86
speedcell4 <speedcell4@gmail.com>
2022-11-17 00:46:34 +0900

    Feat: Add batch_first_filling_mask*

--- torchrua/utils.py
+++ torchrua/utils.py
@@ -1,48 +1,84 @@
 from functools import singledispatch
 from typing import Tuple, Union
 
+import torch
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
 from torch.types import Device
 
-from torchrua import CattedSequence, major_sizes_to_size, major_sizes_to_ptr
+from torchrua.catting import CattedSequence
+from torchrua.core import major_sizes_to_size, major_sizes_to_ptr
 
 __all__ = [
-    'sequence_shape', 'catted_sequence_shape', 'packed_sequence_shape',
-    'sequence_ptr', 'catted_sequence_ptr', 'packed_sequence_ptr',
+    'sequence_shape',
+    'batch_first_filling_mask_shape',
+    'catted_sequence_shape', 'packed_sequence_shape',
+
+    'sequence_ptr',
+    'batch_first_filling_mask_ptr',
+    'catted_sequence_ptr', 'packed_sequence_ptr',
 ]
 
-Sequence = Union[CattedSequence, PackedSequence]
+Sequence = Union[Tensor, CattedSequence, PackedSequence]
+
+Shape = Tuple[int, int, int]
 
 
 @singledispatch
-def sequence_shape(sequence: Sequence, batch_first: bool = True) -> Tuple[int, int, int]:
+def sequence_shape(sequence: Sequence, batch_first: bool = True) -> Shape:
     raise TypeError(f'type {type(sequence)} is not supported')
 
 
 @sequence_shape.register
-def catted_sequence_shape(sequence: CattedSequence, batch_first: bool = True) -> Tuple[int, int, int]:
+def batch_first_filling_mask_shape(sequence: Tensor, batch_first: bool = True) -> Shape:
+    assert sequence.dtype is torch.bool
+    assert sequence.dim() == 2
+
+    b, t = sequence.size()
+    n = sequence.long().sum().item()
+    return (n, b, t) if batch_first else (n, t, b)
+
+
+@sequence_shape.register
+def catted_sequence_shape(sequence: CattedSequence, batch_first: bool = True) -> Shape:
     t, b = major_sizes_to_size(sizes=sequence.token_sizes)
     n, *_ = sequence.data.size()
     return (n, b, t) if batch_first else (n, t, b)
 
 
 @sequence_shape.register
-def packed_sequence_shape(sequence: PackedSequence, batch_first: bool = True) -> Tuple[int, int, int]:
+def packed_sequence_shape(sequence: PackedSequence, batch_first: bool = True) -> Shape:
     b, t = major_sizes_to_size(sizes=sequence.batch_sizes)
     n, *_ = sequence.data.size()
     return (n, b, t) if batch_first else (n, t, b)
 
 
+Ptr = Tuple[Tensor, Tensor]
+
+
 @singledispatch
-def sequence_ptr(sequence: Sequence, batch_first: bool = True,
-                 device: Device = None) -> Tuple[Tensor, Tensor]:
+def sequence_ptr(sequence: Sequence, batch_first: bool = True, device: Device = None) -> Ptr:
     raise TypeError(f'type {type(sequence)} is not supported')
 
 
 @sequence_ptr.register
-def catted_sequence_ptr(sequence: CattedSequence, batch_first: bool = True,
-                        device: Device = None) -> Tuple[Tensor, Tensor]:
+def batch_first_filling_mask_ptr(sequence: Tensor, batch_first: bool = True, device: Device = None) -> Ptr:
+    assert sequence.dtype is torch.bool
+    assert sequence.dim() == 2
+
+    if device is None:
+        device = sequence.device
+
+    b, t = sequence.size()
+    index1 = torch.arange(b, dtype=torch.long, device=device)
+    index2 = torch.arange(t, dtype=torch.long, device=device)
+    batch_ptr = torch.masked_select(index1[:, None], mask=sequence)
+    token_ptr = torch.masked_select(index2[None, :], mask=sequence)
+    return (batch_ptr, token_ptr) if batch_first else (token_ptr, batch_ptr)
+
+
+@sequence_ptr.register
+def catted_sequence_ptr(sequence: CattedSequence, batch_first: bool = True, device: Device = None) -> Ptr:
     if device is None:
         device = sequence.data.device
 
@@ -51,8 +87,7 @@ def catted_sequence_ptr(sequence: CattedSequence, batch_first: bool = True,
 
 
 @sequence_ptr.register
-def packed_sequence_ptr(sequence: PackedSequence, batch_first: bool = True,
-                        device: Device = None) -> Tuple[Tensor, Tensor]:
+def packed_sequence_ptr(sequence: PackedSequence, batch_first: bool = True, device: Device = None) -> Ptr:
     if device is None:
         device = sequence.data.device
 
