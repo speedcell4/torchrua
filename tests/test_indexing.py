97e84200c74a62423e0e5c33b1e5bc64232f3f27
speedcell4 <speedcell4@gmail.com>
2023-02-17 21:15:48 +0900

    Refactor: Update indexing.py

--- tests/test_indexing.py
+++ tests/test_indexing.py
@@ -1,11 +1,12 @@
 import torch
 from hypothesis import given, strategies as st
 
-from tests.assertion import assert_packed_sequence_close, assert_close, assert_grad_close, assert_catted_sequence_close
-from tests.strategy import sizes, device, EMBEDDING_DIM, BATCH_SIZE, TOKEN_SIZE
+from tests.assertion import assert_catted_sequence_close, assert_packed_sequence_close, assert_close, assert_grad_close
+from tests.strategy import sizes, device, BATCH_SIZE, TOKEN_SIZE, EMBEDDING_DIM
 from torchrua.catting import cat_sequence
-from torchrua.indexing import head_catted_sequence, last_catted_sequence, init_sequence, tail_sequence
+from torchrua.indexing import head_catted_sequence, last_catted_sequence
 from torchrua.indexing import head_packed_sequence, last_packed_sequence
+from torchrua.indexing import init_sequence, tail_sequence
 from torchrua.packing import pack_sequence
 
 
@@ -14,16 +15,16 @@ from torchrua.packing import pack_sequence
     dim=sizes(EMBEDDING_DIM),
 )
 def test_head_catted_sequence(token_sizes, dim):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    actual = head_catted_sequence(sequence=cat_sequence(sequences))
-    expected = torch.stack([sequence[0] for sequence in sequences], dim=0)
+    actual = head_catted_sequence(sequence=cat_sequence(inputs))
+    expected = torch.stack([sequence[0] for sequence in inputs], dim=0)
 
     assert_close(actual=actual, expected=expected)
-    assert_grad_close(actual=actual, expected=expected, inputs=sequences)
+    assert_grad_close(actual=actual, expected=expected, inputs=inputs)
 
 
 @given(
@@ -31,16 +32,16 @@ def test_head_catted_sequence(token_sizes, dim):
     dim=sizes(EMBEDDING_DIM),
 )
 def test_last_catted_sequence(token_sizes, dim):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    actual = last_catted_sequence(sequence=cat_sequence(sequences))
-    expected = torch.stack([sequence[-1] for sequence in sequences], dim=0)
+    actual = last_catted_sequence(sequence=cat_sequence(inputs))
+    expected = torch.stack([sequence[-1] for sequence in inputs], dim=0)
 
     assert_close(actual=actual, expected=expected)
-    assert_grad_close(actual=actual, expected=expected, inputs=sequences)
+    assert_grad_close(actual=actual, expected=expected, inputs=inputs)
 
 
 @given(
@@ -51,16 +52,16 @@ def test_last_catted_sequence(token_sizes, dim):
 def test_init_catted_sequence(data, token_sizes, dim):
     n = data.draw(st.integers(min_value=1, max_value=min(token_sizes)))
 
-    sequences = [
+    inputs = [
         torch.randn((token_size + 1, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    actual = init_sequence(cat_sequence(sequences), n=n)
-    expected = cat_sequence([sequence[:-n] for sequence in sequences])
+    actual = init_sequence(cat_sequence(inputs), n=n)
+    expected = cat_sequence([sequence[:-n] for sequence in inputs])
 
     assert_catted_sequence_close(actual=actual, expected=expected)
-    assert_grad_close(actual=actual.data, expected=expected.data, inputs=sequences)
+    assert_grad_close(actual=actual.data, expected=expected.data, inputs=inputs)
 
 
 @given(
@@ -71,16 +72,16 @@ def test_init_catted_sequence(data, token_sizes, dim):
 def test_tail_catted_sequence(data, token_sizes, dim):
     n = data.draw(st.integers(min_value=1, max_value=min(token_sizes)))
 
-    sequences = [
+    inputs = [
         torch.randn((token_size + 1, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    actual = tail_sequence(cat_sequence(sequences), n=n)
-    expected = cat_sequence([sequence[n:] for sequence in sequences])
+    actual = tail_sequence(cat_sequence(inputs), n=n)
+    expected = cat_sequence([sequence[n:] for sequence in inputs])
 
     assert_catted_sequence_close(actual=actual, expected=expected)
-    assert_grad_close(actual=actual.data, expected=expected.data, inputs=sequences)
+    assert_grad_close(actual=actual.data, expected=expected.data, inputs=inputs)
 
 
 @given(
@@ -89,20 +90,20 @@ def test_tail_catted_sequence(data, token_sizes, dim):
     unsort=st.booleans(),
 )
 def test_head_packed_sequence(token_sizes, dim, unsort):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    packed_sequence = pack_sequence(sequences)
-    actual = head_packed_sequence(sequence=packed_sequence, unsort=unsort)
+    sequence = pack_sequence(inputs)
+    actual = head_packed_sequence(sequence=sequence, unsort=unsort)
     if not unsort:
-        actual = actual[packed_sequence.unsorted_indices]
+        actual = actual[sequence.unsorted_indices]
 
-    expected = torch.stack([sequence[0] for sequence in sequences], dim=0)
+    expected = torch.stack([sequence[0] for sequence in inputs], dim=0)
 
     assert_close(actual=actual, expected=expected)
-    assert_grad_close(actual=actual, expected=expected, inputs=sequences)
+    assert_grad_close(actual=actual, expected=expected, inputs=inputs)
 
 
 @given(
@@ -111,20 +112,20 @@ def test_head_packed_sequence(token_sizes, dim, unsort):
     unsort=st.booleans(),
 )
 def test_last_packed_sequence(token_sizes, dim, unsort):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    packed_sequence = pack_sequence(sequences)
-    actual = last_packed_sequence(sequence=packed_sequence, unsort=unsort)
+    sequence = pack_sequence(inputs)
+    actual = last_packed_sequence(sequence=sequence, unsort=unsort)
     if not unsort:
-        actual = actual[packed_sequence.unsorted_indices]
+        actual = actual[sequence.unsorted_indices]
 
-    expected = torch.stack([sequence[-1] for sequence in sequences], dim=0)
+    expected = torch.stack([sequence[-1] for sequence in inputs], dim=0)
 
     assert_close(actual=actual, expected=expected)
-    assert_grad_close(actual=actual, expected=expected, inputs=sequences)
+    assert_grad_close(actual=actual, expected=expected, inputs=inputs)
 
 
 @given(
@@ -135,15 +136,15 @@ def test_last_packed_sequence(token_sizes, dim, unsort):
 def test_init_packed_sequence(data, token_sizes, dim):
     n = data.draw(st.integers(min_value=1, max_value=min(token_sizes)))
 
-    sequences = [
+    inputs = [
         torch.randn((token_size + 1, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
-    actual = init_sequence(pack_sequence(sequences), n=n)
-    expected = pack_sequence([sequence[:-n] for sequence in sequences])
+    actual = init_sequence(pack_sequence(inputs), n=n)
+    expected = pack_sequence([sequence[:-n] for sequence in inputs])
 
     assert_packed_sequence_close(actual=actual, expected=expected)
-    assert_grad_close(actual=actual.data, expected=expected.data, inputs=sequences)
+    assert_grad_close(actual=actual.data, expected=expected.data, inputs=inputs)
 
 
 @given(
@@ -154,13 +155,13 @@ def test_init_packed_sequence(data, token_sizes, dim):
 def test_tail_packed_sequence(data, token_sizes, dim):
     n = data.draw(st.integers(min_value=1, max_value=min(token_sizes)))
 
-    sequences = [
+    inputs = [
         torch.randn((token_size + 1, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    actual = tail_sequence(pack_sequence(sequences), n=n)
-    expected = pack_sequence([sequence[n:] for sequence in sequences])
+    actual = tail_sequence(pack_sequence(inputs), n=n)
+    expected = pack_sequence([sequence[n:] for sequence in inputs])
 
     assert_packed_sequence_close(actual=actual, expected=expected)
-    assert_grad_close(actual=actual.data, expected=expected.data, inputs=sequences)
+    assert_grad_close(actual=actual.data, expected=expected.data, inputs=inputs)
