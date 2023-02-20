6e0e55bc63e3d00ebbf76cb6ad7b79b728bb47c3
speedcell4 <speedcell4@gmail.com>
2023-02-20 22:17:27 +0900

    Refactor: Update packing.py

--- tests/test_packing.py
+++ tests/test_packing.py
@@ -1,11 +1,11 @@
 import torch
 from hypothesis import given, strategies as st
-from torch.nn.utils.rnn import pack_sequence as torch_pack_sequence
-from torch.nn.utils.rnn import pad_sequence as torch_pad_sequence
+from torch.nn.utils.rnn import pack_sequence as excepted_pack_sequence
+from torch.nn.utils.rnn import pad_sequence as excepted_pad_sequence
 
 from tests.assertion import assert_packed_sequence_close, assert_grad_close
-from tests.strategy import device, sizes, EMBEDDING_DIM, BATCH_SIZE, TOKEN_SIZE
-from torchrua import pack_sequence, pack_padded_sequence
+from tests.strategy import sizes, device, BATCH_SIZE, TOKEN_SIZE, EMBEDDING_DIM
+from torchrua import pack_sequence, pack_padded_sequence, cat_sequence, pack_catted_sequence
 
 
 @given(
@@ -14,16 +14,35 @@ from torchrua import pack_sequence, pack_padded_sequence
     dim=sizes(EMBEDDING_DIM),
 )
 def test_pack_sequence(data, token_sizes, dim):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    actual = pack_sequence(sequences, device=device)
-    excepted = torch_pack_sequence(sequences, enforce_sorted=False)
+    actual = pack_sequence(inputs, device=device)
+    excepted = excepted_pack_sequence(inputs, enforce_sorted=False)
 
     assert_packed_sequence_close(actual=actual, expected=excepted)
-    assert_grad_close(actual=actual.data, expected=excepted.data, inputs=sequences)
+    assert_grad_close(actual=actual.data, expected=excepted.data, inputs=inputs)
+
+
+@given(
+    data=st.data(),
+    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
+    dim=sizes(EMBEDDING_DIM),
+)
+def test_pack_catted_sequence(data, token_sizes, dim):
+    inputs = [
+        torch.randn((token_size, dim), device=device, requires_grad=True)
+        for token_size in token_sizes
+    ]
+
+    actual = pack_catted_sequence(cat_sequence(inputs))
+
+    excepted = excepted_pack_sequence(inputs, enforce_sorted=False)
+
+    assert_packed_sequence_close(actual=actual, expected=excepted)
+    assert_grad_close(actual=actual.data, expected=excepted.data, inputs=inputs)
 
 
 @given(
@@ -33,18 +52,18 @@ def test_pack_sequence(data, token_sizes, dim):
     batch_first=st.booleans(),
 )
 def test_pack_padded_sequence(data, token_sizes, dim, batch_first):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
     actual = pack_padded_sequence(
-        torch_pad_sequence(sequences, batch_first=batch_first),
+        excepted_pad_sequence(inputs, batch_first=batch_first),
         torch.tensor(token_sizes, device=device),
         batch_first=batch_first,
     )
 
-    excepted = torch_pack_sequence(sequences, enforce_sorted=False)
+    excepted = excepted_pack_sequence(inputs, enforce_sorted=False)
 
     assert_packed_sequence_close(actual=actual, expected=excepted)
-    assert_grad_close(actual=actual.data, expected=excepted.data, inputs=sequences)
+    assert_grad_close(actual=actual.data, expected=excepted.data, inputs=inputs)
--- tests/test_padding.py
+++ tests/test_padding.py
@@ -34,22 +34,22 @@ def test_pad_sequence(data, token_sizes, dim, batch_first):
     batch_first=st.booleans(),
 )
 def test_pad_catted_sequence(data, token_sizes, dim, batch_first):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
     actual, actual_token_sizes = pad_sequence(
-        cat_sequence(sequences),
+        cat_sequence(inputs),
         batch_first=batch_first,
     )
 
-    excepted = excepted_pad_sequence(sequences, batch_first=batch_first)
+    excepted = excepted_pad_sequence(inputs, batch_first=batch_first)
     excepted_token_sizes = torch.tensor(token_sizes, device=device)
 
     assert_close(actual=actual, expected=excepted)
     assert_close(actual=actual_token_sizes, expected=excepted_token_sizes)
-    assert_grad_close(actual=actual, expected=excepted, inputs=sequences)
+    assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
 
 
 @given(
@@ -59,19 +59,19 @@ def test_pad_catted_sequence(data, token_sizes, dim, batch_first):
     batch_first=st.booleans(),
 )
 def test_pad_packed_sequence(data, token_sizes, dim, batch_first):
-    sequences = [
+    inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
     actual, actual_token_sizes = pad_sequence(
-        excepted_pack_sequence(sequences, enforce_sorted=False),
+        excepted_pack_sequence(inputs, enforce_sorted=False),
         batch_first=batch_first,
     )
 
-    excepted = excepted_pad_sequence(sequences, batch_first=batch_first)
+    excepted = excepted_pad_sequence(inputs, batch_first=batch_first)
     excepted_token_sizes = torch.tensor(token_sizes, device=device)
 
     assert_close(actual=actual, expected=excepted)
     assert_close(actual=actual_token_sizes, expected=excepted_token_sizes)
-    assert_grad_close(actual=actual, expected=excepted, inputs=sequences)
+    assert_grad_close(actual=actual, expected=excepted, inputs=inputs)
