8cac7e5d04beed6d4689f794a8f5052fbee157e1
speedcell4 <speedcell4@gmail.com>
2023-08-11 20:09:53 +0900

    Feat: Update catting.py

--- torchrua/catting.py
+++ torchrua/catting.py
@@ -3,10 +3,11 @@ from typing import List
 import torch
 from torch import Tensor
 from torch.nn.utils.rnn import PackedSequence
-from torch.types import Device
 
 from torchrua.core import CattedSequence
 from torchrua.core import accumulate_sizes
+from torchrua.core import broadcast_devices
+from torchrua.core import get_device
 from torchrua.core import major_sizes_to_ptr
 from torchrua.core import minor_sizes_to_ptr
 
@@ -17,56 +18,44 @@ __all__ = [
 ]
 
 
-def cat_sequence(sequences: List[Tensor], dtype: torch.dtype = None, device: Device = None) -> CattedSequence:
-    if device is None:
-        device = sequences[0].device
+def cat_sequence(sequences: List[Tensor], dtype: torch.dtype = None, device: torch.device = None):
+    device = get_device(*sequences, device=device)
 
     return CattedSequence(
         data=torch.cat(sequences, dim=0).to(dtype=dtype, device=device),
-        token_sizes=torch.tensor([seq.size()[0] for seq in sequences], dtype=torch.long, device=device),
+        token_sizes=torch.tensor([sequence.size()[0] for sequence in sequences], dtype=torch.long, device=device),
     )
 
 
-@torch.no_grad()
-def cat_packed_indices(batch_sizes: Tensor, unsorted_indices: Tensor, device: Device = None):
-    if device is None:
-        if unsorted_indices is not None:
-            device = unsorted_indices.device
-        elif batch_sizes is not None:
-            device = batch_sizes.device
-        else:
-            raise RuntimeError('batch_sizes and unsorted_indices are all None')
+def cat_packed_indices(batch_sizes: Tensor, unsorted_indices: Tensor, device: torch.device = None):
+    unsorted_indices, batch_sizes, device = broadcast_devices(unsorted_indices, batch_sizes, device=device)
 
-    batch_sizes = batch_sizes.to(device=device)
     acc_batch_sizes = accumulate_sizes(sizes=batch_sizes)
-
     batch_ptr, token_ptr, token_sizes = minor_sizes_to_ptr(sizes=batch_sizes, minor_ptr=unsorted_indices)
 
     return batch_ptr + acc_batch_sizes[token_ptr], token_sizes
 
 
-def cat_packed_sequence(sequence: PackedSequence, device: Device = None) -> CattedSequence:
-    if device is None:
-        device = sequence.data.device
+def cat_packed_sequence(sequence: PackedSequence, device: torch.device = None) -> CattedSequence:
+    sequence, batch_sizes, sorted_indices, unsorted_indices, device = broadcast_devices(
+        *sequence, device=device,
+    )
 
     indices, token_sizes = cat_packed_indices(
-        batch_sizes=sequence.batch_sizes,
-        unsorted_indices=sequence.unsorted_indices,
+        batch_sizes=batch_sizes,
+        unsorted_indices=unsorted_indices,
         device=device,
     )
 
     return CattedSequence(
-        data=sequence.data[indices],
+        data=sequence[indices],
         token_sizes=token_sizes,
     )
 
 
-@torch.no_grad()
-def cat_padded_indices(token_sizes: Tensor, batch_first: bool, device: Device = None):
-    if device is None:
-        device = token_sizes.device
+def cat_padded_indices(token_sizes: Tensor, batch_first: bool, device: torch.device = None):
+    token_sizes, device = broadcast_devices(token_sizes, device=device)
 
-    token_sizes = token_sizes.to(device=device)
     token_ptr, batch_ptr = major_sizes_to_ptr(sizes=token_sizes)
 
     if batch_first:
@@ -75,10 +64,8 @@ def cat_padded_indices(token_sizes: Tensor, batch_first: bool, device: Device = 
         return (token_ptr, batch_ptr), token_sizes
 
 
-def cat_padded_sequence(sequence: Tensor, token_sizes: Tensor,
-                        batch_first: bool = False, device: Device = None) -> CattedSequence:
-    if device is None:
-        device = sequence.device
+def cat_padded_sequence(sequence: Tensor, token_sizes: Tensor, batch_first: bool = False, device: torch.device = None):
+    sequence, token_sizes, device = broadcast_devices(sequence, token_sizes, device=device)
 
     indices, token_sizes = cat_padded_indices(
         token_sizes=token_sizes,
