9019f107af0557d4adce7cdba64caadbfd317ec8
speedcell4 <speedcell4@gmail.com>
2023-08-24 00:14:21 +0900

    Test: Update test_roll_sequence

--- tests/test_roll.py
+++ tests/test_roll.py
@@ -6,7 +6,6 @@ from torchnyan import FEATURE_DIM
 from torchnyan import TOKEN_SIZE
 from torchnyan import assert_catted_sequence_close
 from torchnyan import assert_grad_close
-from torchnyan import assert_packed_sequence_close
 from torchnyan import device
 from torchnyan import sizes
 
@@ -19,60 +18,18 @@ from torchrua import pad_sequence
     data=st.data(),
     token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
     dim=sizes(FEATURE_DIM),
+    rua_sequence=st.sampled_from([cat_sequence, pad_sequence, pack_sequence]),
 )
-def test_roll_catted_sequence(data, token_sizes, dim):
+def test_roll_sequence(data, token_sizes, dim, rua_sequence):
     shifts = data.draw(st.integers(min_value=-max(token_sizes), max_value=+max(token_sizes)))
 
     inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
-    sequence = cat_sequence(inputs)
 
-    actual = sequence.roll(shifts=shifts)
+    actual = rua_sequence(inputs).roll(shifts=shifts).cat()
     expected = cat_sequence([sequence.roll(shifts, dims=[0]) for sequence in inputs])
 
     assert_catted_sequence_close(actual, expected)
-    assert_grad_close(actual.data, expected.data, inputs=inputs)
-
-
-@given(
-    data=st.data(),
-    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
-    dim=sizes(FEATURE_DIM),
-)
-def test_roll_padded_sequence(data, token_sizes, dim):
-    shifts = data.draw(st.integers(min_value=-max(token_sizes), max_value=+max(token_sizes)))
-
-    inputs = [
-        torch.randn((token_size, dim), device=device, requires_grad=True)
-        for token_size in token_sizes
-    ]
-    sequence = pad_sequence(inputs)
-
-    actual = sequence.roll(shifts=shifts).cat()
-    expected = cat_sequence([sequence.roll(shifts, dims=[0]) for sequence in inputs])
-
-    assert_catted_sequence_close(actual, expected)
-    assert_grad_close(actual.data, expected.data, inputs=inputs)
-
-
-@given(
-    data=st.data(),
-    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
-    dim=sizes(FEATURE_DIM),
-)
-def test_roll_packed_sequence(data, token_sizes, dim):
-    shifts = data.draw(st.integers(min_value=-max(token_sizes), max_value=+max(token_sizes)))
-
-    inputs = [
-        torch.randn((token_size, dim), device=device, requires_grad=True)
-        for token_size in token_sizes
-    ]
-    sequence = pack_sequence(inputs)
-
-    actual = sequence.roll(shifts=shifts)
-    expected = pack_sequence([sequence.roll(shifts, dims=[0]) for sequence in inputs])
-
-    assert_packed_sequence_close(actual, expected)
     assert_grad_close(actual.data, expected.data, inputs=inputs)
