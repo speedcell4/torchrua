f90e11c48cd386e7d43f35c56f3f4a6c1c70dfae
speedcell4 <speedcell4@gmail.com>
2023-08-24 00:13:31 +0900

    Test: Update test_trunc_sequence

--- tests/test_trunc.py
+++ tests/test_trunc.py
@@ -6,7 +6,6 @@ from torchnyan import FEATURE_DIM
 from torchnyan import TOKEN_SIZE
 from torchnyan import assert_catted_sequence_close
 from torchnyan import assert_grad_close
-from torchnyan import assert_packed_sequence_close
 from torchnyan import device
 from torchnyan import sizes
 
@@ -19,63 +18,20 @@ from torchrua import pad_sequence
     data=st.data(),
     token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
     dim=sizes(FEATURE_DIM),
+    rua_sequence=st.sampled_from([cat_sequence, pad_sequence, pack_sequence]),
 )
-def test_trunc_catted_sequence(data, token_sizes, dim):
+def test_trunc_sequence(data, token_sizes, dim, rua_sequence):
     inputs = [
         torch.randn((token_size, dim), device=device, requires_grad=True)
         for token_size in token_sizes
     ]
 
-    s = min(token_sizes)
+    s = min(token_sizes) - 1
     a = data.draw(st.integers(0, max_value=s))
     b = data.draw(st.integers(0, max_value=s - a))
 
-    actual = cat_sequence(inputs).trunc((a, b))
+    actual = rua_sequence(inputs).trunc((a, b)).cat()
     excepted = cat_sequence([sequence[a:sequence.size()[0] - b] for sequence in inputs])
 
     assert_catted_sequence_close(actual=actual, expected=excepted)
-    assert_grad_close(actual=actual.data, expected=excepted.data, inputs=inputs)
-
-
-@given(
-    data=st.data(),
-    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
-    dim=sizes(FEATURE_DIM),
-)
-def test_trunc_padded_sequence(data, token_sizes, dim):
-    inputs = [
-        torch.randn((token_size, dim), device=device, requires_grad=True)
-        for token_size in token_sizes
-    ]
-
-    s = min(token_sizes) - 1  # TODO: support zero-length packing
-    a = data.draw(st.integers(0, max_value=s))
-    b = data.draw(st.integers(0, max_value=s - a))
-
-    actual = pad_sequence(inputs).trunc((a, b)).pack()
-    excepted = pack_sequence([sequence[a:sequence.size()[0] - b] for sequence in inputs])
-
-    assert_packed_sequence_close(actual=actual, expected=excepted)
-    assert_grad_close(actual=actual.data, expected=excepted.data, inputs=inputs)
-
-
-@given(
-    data=st.data(),
-    token_sizes=sizes(BATCH_SIZE, TOKEN_SIZE),
-    dim=sizes(FEATURE_DIM),
-)
-def test_trunc_packed_sequence(data, token_sizes, dim):
-    inputs = [
-        torch.randn((token_size, dim), device=device, requires_grad=True)
-        for token_size in token_sizes
-    ]
-
-    s = min(token_sizes) - 1  # TODO: support zero-length packing
-    a = data.draw(st.integers(0, max_value=s))
-    b = data.draw(st.integers(0, max_value=s - a))
-
-    actual = pack_sequence(inputs).trunc((a, b))
-    excepted = pack_sequence([sequence[a:sequence.size()[0] - b] for sequence in inputs])
-
-    assert_packed_sequence_close(actual=actual, expected=excepted)
     assert_grad_close(actual=actual.data, expected=excepted.data, inputs=inputs)
