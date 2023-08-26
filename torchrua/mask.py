3ca450b0309e2521acc7ac70f802db436af8b55c
speedcell4 <speedcell4@gmail.com>
2023-08-26 14:55:06 +0900

    Feat: Update mask_cdp

--- torchrua/mask.py
+++ torchrua/mask.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 import torch
+from torch.types import Number
 
 from torchrua.ty import C
 from torchrua.ty import D
@@ -8,12 +9,12 @@ from torchrua.ty import P
 from torchrua.ty import T
 
 
-def mask_cdp(sequence: Union[C, D, P]) -> T:
+def mask_cdp(sequence: Union[C, D, P], zero: Number = False, one: Number = True, dtype: torch.dtype = torch.long) -> T:
     b, t, *_ = sequence.size()
     batch_ptr, token_ptr = sequence.ptr()
 
-    mask = sequence.data.new_zeros((b, t), dtype=torch.bool)
-    mask[batch_ptr, token_ptr] = True
+    mask = sequence.data.new_full((b, t), fill_value=zero, dtype=dtype)
+    mask[batch_ptr, token_ptr] = one
     return mask
 
 
