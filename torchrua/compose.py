53df2f992894f57e48bfc814c6de07d418b87a53
speedcell4 <speedcell4@gmail.com>
2023-08-27 20:42:56 +0900

    Style: Update compose

--- torchrua/compose.py
+++ torchrua/compose.py
@@ -10,27 +10,27 @@ from torchrua.core import invert_permutation
 
 
 def compose(sequences: List[Union[C, D, P]]) -> P:
-    offset, data, idx, token_sizes = 0, [], [], []
+    offset, data, indices, token_sizes = 0, [], [], []
 
-    for seq in sequences:
-        data.append(seq._data())
+    for sequence in sequences:
+        data.append(sequence._data())
 
-        i, t = seq.idx().cat()
-        idx.append(i + offset)
-        token_sizes.append(t)
+        idx, sizes = sequence.idx().cat()
+        indices.append(idx + offset)
+        token_sizes.append(sizes)
 
         offset += data[-1].size()[0]
 
     token_sizes = C.new(token_sizes)
     unsorted_indices, _, _, _ = token_sizes.idx().pack()
 
-    idx = C(data=torch.cat(idx, dim=0), token_sizes=token_sizes.data).pack()
-    unsorted_indices = idx.unsorted_indices[unsorted_indices]
+    indices = C(data=torch.cat(indices, dim=0), token_sizes=token_sizes.data).pack()
+    unsorted_indices = indices.unsorted_indices[unsorted_indices]
     sorted_indices = invert_permutation(unsorted_indices)
 
-    idx = idx._replace(
+    indices = indices._replace(
         sorted_indices=sorted_indices,
         unsorted_indices=unsorted_indices,
     )
 
-    return idx.rua(torch.cat(data, dim=0))
+    return indices.rua(torch.cat(data, dim=0))
