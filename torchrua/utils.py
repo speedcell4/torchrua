860a0b93c4a862d361ba6747faf7c8203bf0de59
speedcell4 <speedcell4@gmail.com>
2024-02-17 00:00:55 +0900

    Feat: Add broadcast_shapes

--- torchrua/utils.py
+++ torchrua/utils.py
@@ -7,3 +7,8 @@ def with_shape(shape: torch.Size, dim: int, value: int) -> List[int]:
     shape = list(shape)
     shape[dim] = value
     return shape
+
+
+def broadcast_shapes(*sizes: torch.Size, dim: int):
+    shape = torch.broadcast_shapes(*[with_shape(size, dim=dim, value=1) for size in sizes])
+    return [with_shape(shape, dim=dim, value=size[dim]) for size in sizes]
