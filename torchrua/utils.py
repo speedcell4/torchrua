2d6b690fd68ca603e1550eafba33fc7725f521ca
speedcell4 <speedcell4@gmail.com>
2024-02-17 00:01:06 +0900

    Feat: Add broadcast_tensors

--- torchrua/utils.py
+++ torchrua/utils.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import torch
+from torch import Tensor
 
 
 def with_shape(shape: torch.Size, dim: int, value: int) -> List[int]:
@@ -12,3 +13,8 @@ def with_shape(shape: torch.Size, dim: int, value: int) -> List[int]:
 def broadcast_shapes(*sizes: torch.Size, dim: int):
     shape = torch.broadcast_shapes(*[with_shape(size, dim=dim, value=1) for size in sizes])
     return [with_shape(shape, dim=dim, value=size[dim]) for size in sizes]
+
+
+def broadcast_tensors(*tensors: Tensor, dim: int):
+    shapes = broadcast_shapes(*[tensor.size() for tensor in tensors], dim=dim)
+    return [tensor.expand(shape) for tensor, shape in zip(tensors, shapes)]
