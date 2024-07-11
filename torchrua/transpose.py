a1399a774e50f133a34403c7fb1e38ceb3bfd4fb
speedcell4 <3585459+speedcell4@users.noreply.github.com>
2024-07-11 20:58:28 +0900

    Feat: Add lazy property transpose.py

--- torchrua/transpose.py
+++ torchrua/transpose.py
@@ -9,26 +9,32 @@ from torchrua.core import invert_permutation
 
 
 def token_sizes(self: P) -> Tensor:
-    b, t, *_ = self.size()
-    batch_ptr, token_ptr = self.ptr()
+    if not hasattr(self, '_token_sizes'):
+        b, t, *_ = self.size()
+        batch_ptr, token_ptr = self.ptr()
 
-    mask = self.data.new_zeros((b, t), dtype=torch.long)
-    mask[batch_ptr, token_ptr] = 1
+        mask = self.data.new_zeros((b, t), dtype=torch.long)
+        mask[batch_ptr, token_ptr] = 1
 
-    return mask.sum(dim=1)
+        setattr(self, '_token_sizes', mask.sum(dim=1))
+
+    return getattr(self, '_token_sizes')
 
 
 P.token_sizes = lazy_property(token_sizes)
 
 
 def batch_sizes(self: Union[C, L, R]) -> Tensor:
-    b, t, *_ = self.size()
-    batch_ptr, token_ptr = self.ptr()
+    if not hasattr(self, '_batch_sizes'):
+        b, t, *_ = self.size()
+        batch_ptr, token_ptr = self.ptr()
 
-    mask = self.data.new_zeros((b, t), dtype=torch.long)
-    mask[batch_ptr, token_ptr] = 1
+        mask = self.data.new_zeros((b, t), dtype=torch.long)
+        mask[batch_ptr, token_ptr] = 1
 
-    return mask.sum(dim=0)
+        setattr(self, '_batch_sizes', mask.sum(dim=0))
+
+    return getattr(self, '_batch_sizes')
 
 
 C.batch_sizes = lazy_property(batch_sizes)
@@ -37,8 +43,11 @@ R.batch_sizes = lazy_property(batch_sizes)
 
 
 def sorted_indices(self: Union[C, L, R]) -> Tensor:
-    _, index = torch.sort(self.token_sizes.detach().cpu(), descending=True)
-    return index.to(device=self.data.device)
+    if not hasattr(self, '_sorted_indices'):
+        _, index = torch.sort(self.token_sizes.detach().cpu(), descending=True)
+        setattr(self, '_sorted_indices', index.to(device=self.data.device))
+
+    return getattr(self, '_sorted_indices')
 
 
 C.sorted_indices = lazy_property(sorted_indices)
@@ -47,7 +56,10 @@ R.sorted_indices = lazy_property(sorted_indices)
 
 
 def unsorted_indices(self: Union[C, L, R]) -> Tensor:
-    return invert_permutation(self.sorted_indices)
+    if not hasattr(self, '_unsorted_indices'):
+        setattr(self, '_unsorted_indices', invert_permutation(self.sorted_indices))
+
+    return getattr(self, '_unsorted_indices')
 
 
 C.unsorted_indices = lazy_property(unsorted_indices)
