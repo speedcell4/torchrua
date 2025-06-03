06f23ada0a38f19fb865fde4db6e92414d9d8e67
speedcell4 <3585459+speedcell4@users.noreply.github.com>
2025-06-03 22:52:36 +0900

    Feat: Add cat_head

--- torchrua/select/head.py
+++ torchrua/select/head.py
@@ -4,6 +4,22 @@ from torch import Tensor
 from torchrua.layout import C, L, P, R, Z
 
 
+def cat_head(self: C, n: int = 1) -> C:
+    data, token_sizes1 = self
+    token_sizes0 = torch.full_like(token_sizes1, fill_value=n)
+
+    split_sizes = torch.stack([token_sizes0, token_sizes1 - token_sizes0], dim=-1).view(-1)
+    split_sizes = split_sizes.detach().cpu().tolist()
+
+    data = torch.split_with_sizes(data, split_sizes=split_sizes, dim=0)
+    data = torch.cat(data[0::2], dim=0)
+
+    return C(data=data, token_sizes=token_sizes0)
+
+
+C.head = cat_head
+
+
 def head(self: Z) -> Tensor:
     b, *_ = self.size()
 
@@ -13,7 +29,6 @@ def head(self: Z) -> Tensor:
     return self[batch_ptr, token_ptr]
 
 
-C.head = head
 L.head = head
 P.head = head
 R.head = head
